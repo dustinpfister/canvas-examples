eff8ec89fad2518281cf1b538034f208c6d0a6c9
dustinpfister <dustin.pfister@gmail.com>
2020-04-04 14:45:27 -0400

    deleted old plugins

--- forpost/canvas-example-grid-gradient/plugins/fixed_pos.js
+++
@@ -1,9 +0,0 @@
-gradient.load({
-    objUpdaters: [
-        // fixed position
-        function (grid, obj, secs) {
-            //obj.power = [0, 1, 0];
-            obj.cps = 0;
-        }
-    ]
-});
--- forpost/canvas-example-grid-gradient/plugins/init_updaters_stager.js
+++
@@ -1,9 +0,0 @@
-gradient.load({
-
-    initMethods: {
-        updatersStager: function (obj, grad, i) {
-            //obj.objUpdaterIndex = u.mod(i, grad.objUpdaters.length);
-        }
-    }
-
-});
