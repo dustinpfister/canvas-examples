f5d0617453a5b1cb205c8ea9f97ecc663f61c48a
dustinpfister <dustin.pfister@gmail.com>
2020-04-11 11:59:42 -0400

    main.js clean up

--- forpost/canvas-example-game-kaboom-clone/main.js
+++ forpost/canvas-example-game-kaboom-clone/main.js
@@ -36,53 +36,6 @@ var loop = function () {
     draw.score(ctx, state);
     draw.debug(ctx, state);
 
-    /*
-
-    // draw background
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, kaboom.BOMBER.y, canvas.width, canvas.height - kaboom.BOMBER.y);
-
-    // draw bomber
-    ctx.fillStyle = 'black';
-    ctx.fillRect(state.bomber.x, kaboom.BOMBER.y - 64, kaboom.BOMBER.w, 64);
-
-    // draw bombs
-    var i = state.bombs.length,
-    bomb;
-    ctx.fillStyle = 'red';
-    while (i--) {
-    bomb = state.bombs[i];
-    ctx.fillRect(bomb.x, bomb.y, 32, 32);
-    }
-
-    // draw player
-    ctx.fillStyle = 'lime';
-    ctx.fillRect(state.player.x, kaboom.PLAYER.y, kaboom.PLAYER.w, kaboom.PLAYER.h);
-
-    // draw score
-    ctx.fillStyle = 'white';
-    ctx.font = '10px arial';
-    ctx.textBaseline = 'top';
-    ctx.textAlign = 'center';
-    ctx.fillText(state.score, 320, 20);
-
-    // draw debug info
-    ctx.fillStyle = 'white';
-    ctx.font = '10px arial';
-    ctx.textBaseline = 'top';
-    ctx.textAlign = 'left';
-    ctx.fillText('level: ' + state.level, 10, 10);
-    ctx.fillText('bombCount: ' + state.bombCount, 10, 20);
-    ctx.fillText('bomber: { x: ' + state.bomber.x +
-    ', dir: ' + state.bomber.dir +
-    ', pps: ' + state.bomber.pps + ' }', 10, 30);
-    ctx.fillText('player: { x: ' + state.player.x +
-    ', hp: ' + state.player.hp +
-    ', dir: ' + state.player.dir +
-    ', pps: ' + state.player.pps + ' }', 10, 40);
-     */
 };
 
 loop();
