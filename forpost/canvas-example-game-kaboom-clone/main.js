9502ddb5f17e7ed704b12467782e20a8f81b78a3
dustinpfister <dustin.pfister@gmail.com>
2020-04-11 11:46:12 -0400

    utils.js

--- forpost/canvas-example-game-kaboom-clone/main.js
+++ forpost/canvas-example-game-kaboom-clone/main.js
@@ -1,28 +1,3 @@
-var utils = {}
-// bounding box
-utils.bb = function (a, b) {
-    return !(
-        (a.y + a.h) < b.y ||
-        a.y > (b.y + b.h) ||
-        (a.x + a.w) < b.x ||
-        a.x > (b.x + b.w));
-};
-
-utils.distance = function (x1, y1, x2, y2) {
-    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-};
-
-utils.getCanvasRelative = function (e) {
-    var canvas = e.target,
-    bx = canvas.getBoundingClientRect();
-    var x = (e.changedTouches ? e.changedTouches[0].clientX : e.clientX) - bx.left,
-    y = (e.changedTouches ? e.changedTouches[0].clientY : e.clientY) - bx.top;
-    return {
-        x: x,
-        y: y,
-        bx: bx
-    };
-};
 
 var kaboom = (function () {
 
