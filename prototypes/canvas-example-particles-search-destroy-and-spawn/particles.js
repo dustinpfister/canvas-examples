073d634e5bad037d4c74214d45d08f90bd49104d
dustinpfister <dustin.pfister@gmail.com>
2020-04-13 14:26:28 -0400

    move method

--- prototypes/canvas-example-particles-search-destroy-and-spawn/particles.js
+++ prototypes/canvas-example-particles-search-destroy-and-spawn/particles.js
@@ -10,9 +10,13 @@ var paricles = (function () {
         this.y = opt.y || 0;
         this.heading = opt.heading || 0;
         this.pps = opt.pps || 32; // pixels per second
+        this.type = 'none';
     };
 
-    Particle.prototype.move = function (part) {};
+    Particle.prototype.move = function (part, secs) {
+        this.x += Math.cos(this.heading) * this.pps * secs;
+        this.y += Math.sin(this.heading) * this.pps * secs;
+    };
 
     // public API
     return {
