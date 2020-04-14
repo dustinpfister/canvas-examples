93c7cf759b5a2a0cd289895f15bf8af6f5e7cae7
dustinpfister <dustin.pfister@gmail.com>
2020-04-14 16:54:36 -0400

    started an orbcraft prototype folder

---
+++ prototypes/canvas-example-orbcraft/lib/draw.js
@@ -0,0 +1,10 @@
+var draw = (function () {
+    return {
+        // draw background
+        back: function (ctx, canvas) {
+            ctx.fillStyle = 'black';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+        }
+    }
+}
+    ());
