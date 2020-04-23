d438848d7650ebe89cd360e5d0b6319a938a77b3
dustinpfister <dustin.pfister@gmail.com>
2020-04-23 10:09:53 -0400

    daring pos array

--- forpost/canvas-example-input-controller/lib/draw.js
+++ forpost/canvas-example-input-controller/lib/draw.js
@@ -9,12 +9,13 @@ draw.debugInput = function (ctx, input) {
     ctx.textBaseline = 'top';
     ctx.font = '10px arial';
     ctx.fillText('input.pointerDown: ' + input.pointerDown, 10, 10);
-    var pos = input.pos.length >= 1 ? input.pos : [{
-                x: null,
-                y: null
-            }
-        ];
-    ctx.fillText('input.pos: ' + pos[0].x + ',' + pos[0].x, 10, 20);
+
+    // draw pos points
+    var posPoints = input.pos.map(function (pos) {
+            return pos.x + ',' + pos.y;
+        }).join(' | ');
+    ctx.fillText('input.pos: ' + posPoints, 10, 20);
+
     ctx.fillText('input.keys[87] (w): ' + input.keys[87], 10, 40);
     ctx.fillText('input.keys[65] (a): ' + input.keys[65], 10, 50);
     ctx.fillText('input.keys[83] (s): ' + input.keys[83], 10, 60);
