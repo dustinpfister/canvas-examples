f558e88f6ac3a7d6e8c8a9b5be2a703941a97e88
dustinpfister <dustin.pfister@gmail.com>
2020-04-26 20:21:41 -0400

    worked out an xp system

--- prototypes/canvas-example-game-beach/lib/xp.js
+++ prototypes/canvas-example-game-beach/lib/xp.js
@@ -1,42 +1,39 @@
 var XP = (function () {
-
     // set level with given xp
     var set = function (xp) {
-        return Math.sqrt(xp);
+        //return Math.sqrt(xp);
+        return (1 + Math.sqrt(1 + 8 * xp / 50)) / 2;
     };
     // get exp to the given level with given current_level and xp
     var XPto = function (level) {
-        return Math.pow(level, 2);
+        //return Math.pow(level, 2);
+        return ((Math.pow(level, 2) - level) * 50) / 2;
+    };
+
+    var parseByXP = function (xp) {
+        var l = set(xp),
+        level = Math.floor(l),
+        forNext = XPto(level + 1);
+        return {
+            level: level,
+            levelFrac: l,
+            xp: xp,
+            forNext: forNext,
+            toNext: forNext - xp
+        };
     };
 
     return {
-        set: set,
-        XPto: XPto,
-        parse: function (xp) {
-
-            var l = set(xp),
-            level = Math.floor(l),
-            forNext = XPto(level + 1);
-
-            return {
-                level: level,
-                levelFrac: l,
-                xp: xp,
-                forNext: forNext,
-                toNext: forNext - xp
-            };
-
+        parseByLevel: function (l) {
+            return parseByXP(XPto(l));
         },
-
+        parseByXP: parseByXP
     };
-
 }
     ());
 
-var xp = XP.XPto(10);
-console.log(XP.set(xp)); // level 10
-console.log(xp); // 100 XP
+var obj = XP.parseByXP(750);
+console.log(obj);
 
-var obj = XP.parse(169);
-
+obj = XP.parseByLevel(6);
 console.log(obj);
