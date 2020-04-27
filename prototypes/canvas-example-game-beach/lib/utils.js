00a44088f093f4ee268c6a7476130f21f2bab05c
dustinpfister <dustin.pfister@gmail.com>
2020-04-26 20:23:32 -0400

    made xp system part of utils

--- prototypes/canvas-example-game-beach/lib/utils.js
+++ prototypes/canvas-example-game-beach/lib/utils.js
@@ -3,3 +3,37 @@ var utils = {};
 utils.distance = function (x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
+
+utils.XP = (function () {
+    // set level with given xp
+    var set = function (xp) {
+        //return Math.sqrt(xp);
+        return (1 + Math.sqrt(1 + 8 * xp / 50)) / 2;
+    };
+    // get exp to the given level with given current_level and xp
+    var XPto = function (level) {
+        //return Math.pow(level, 2);
+        return ((Math.pow(level, 2) - level) * 50) / 2;
+    };
+
+    var parseByXP = function (xp) {
+        var l = set(xp),
+        level = Math.floor(l),
+        forNext = XPto(level + 1);
+        return {
+            level: level,
+            levelFrac: l,
+            xp: xp,
+            forNext: forNext,
+            toNext: forNext - xp
+        };
+    };
+
+    return {
+        parseByLevel: function (l) {
+            return parseByXP(XPto(l));
+        },
+        parseByXP: parseByXP
+    };
+}
+    ());
