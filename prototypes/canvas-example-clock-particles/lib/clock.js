d57cee2bfe4d8a628fb927a1637fd91c1dba148d
dustinpfister <dustin.pfister@gmail.com>
2020-05-06 13:28:36 -0400

    started update method for clockMod

--- prototypes/canvas-example-clock-particles/lib/clock.js
+++ prototypes/canvas-example-clock-particles/lib/clock.js
@@ -14,15 +14,25 @@ var clockMod = (function () {
         return new Date(clock.now.getFullYear(), clock.now.getMonth(), clock.now.getDate(), 0, 0, 0, 0)
     };
 
+    var setClockPropsToNow = function (clock) {
+        clock.timeText = getTimeText(clock);
+        var dayStart = getDayStart(clock);
+        clock.dayPer = (clock.now - dayStart) / 86400000;
+        clock.secPer = clock.now.getMilliseconds() / 1000;
+    };
+
     // return a public method that creates a clock object
     return {
-        create: function (date) {
+        create: function (now) {
             var clock = {};
+            clock.now = now || new Date(0);
+            setClockPropsToNow(clock);
+            return clock;
+        },
+
+        update: function (clock, now) {
             clock.now = date || new Date(0);
-            clock.timeText = getTimeText(clock);
-            var dayStart = getDayStart(clock);
-            clock.dayPer = (clock.now - dayStart) / 86400000;
-            clock.secPer = clock.now.getMilliseconds() / 1000;
+            setClockPropsToNow(clock);
             return clock;
         }
     }
--- prototypes/canvas-example-clock-particles/todo.md
+++ prototypes/canvas-example-clock-particles/todo.md
@@ -1,4 +1,4 @@
 ## canvas-example-clock-particles
 
 * have a particles pool for the clock object
-* more particles become active as the day progress to noon
+* more particles become active as the day progress to noon and less as the day progress to midnight
