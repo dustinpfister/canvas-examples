1ca72f7c836e1393642c3059ee712e82d69fe414
dustinpfister <dustin.pfister@gmail.com>
2020-06-07 12:23:36 -0400

    create method for spinner

--- prototypes/canvas-example-game-spinner/lib/spinner.js
+++ prototypes/canvas-example-game-spinner/lib/spinner.js
@@ -2,11 +2,26 @@ var spinner = (function () {
 
     return {
 
+        // create a spinner state object
         create: function (opt) {
-
             opt = opt || {};
-
-        }
+            return {
+                RPS: : {
+                    current: 0,
+                    start: 6,
+                    lossPerSecond: 1
+                },
+                radian: 0,
+                sections: [1, 2, 3, 4, 5],
+                sectionIndexs: [0, 1, 2, 3, 4]
+            }
+        },
+		
+		// start spinning a spinner state object
+		startSpin: function(){
+			
+			
+		};
 
     }
 
