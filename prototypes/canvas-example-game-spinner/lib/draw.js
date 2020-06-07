1bfce7591934b16c4ef43e7afa7ade468af1866e
dustinpfister <dustin.pfister@gmail.com>
2020-06-07 18:54:48 -0400

    more methods for spinner.js

--- prototypes/canvas-example-game-spinner/lib/draw.js
+++ prototypes/canvas-example-game-spinner/lib/draw.js
@@ -1,1 +1,15 @@
 var draw = {};
+
+draw.background = function (ctx, canvas) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+};
+
+draw.sections = function (ctx, spin) {
+
+    var i = spin.secionIndeces.length,
+    section;
+    while (i--) {
+        section = spin.sectionIndeces[i];
+    }
+
+};
--- prototypes/canvas-example-game-spinner/lib/spinner.js
+++ prototypes/canvas-example-game-spinner/lib/spinner.js
@@ -1,27 +1,46 @@
 var spinner = (function () {
 
+    var PI2 = Math.PI * 2;
+
     return {
 
         // create a spinner state object
         create: function (opt) {
             opt = opt || {};
             return {
-                RPS: : {
+                cx: opt.x === undefined ? 0: opt.x,
+                cy: opt.y === undefined ? 0: opt.y,
+                RPS: {
                     current: 0,
                     start: 6,
                     lossPerSecond: 1
                 },
                 radian: 0,
                 sections: [1, 2, 3, 4, 5],
-                sectionIndexs: [0, 1, 2, 3, 4]
+                sectionIndices: [0, 1, 2, 3, 4]
             }
         },
-		
-		// start spinning a spinner state object
-		startSpin: function(){
-			
-			
-		};
+
+        // get current section value or object
+        get: function (spin) {
+            var len = sectionIndices.length,
+            index = Math.floor(spin.radian / PI2 * len);
+            return spin.sections[index];
+        },
+
+        // start spinning a spinner state object
+        startSpin: function (spin) {
+            var RPS = spin.RPS;
+            RPS.current = RPS.start;
+        },
+
+        update: function (spin, secs) {
+            var RPS = spin.RPS;
+            // just step by RPS times secs
+            spin.radian += RPS.current * secs;
+            RPS.current -= RPS.lossPerSecond * secs;
+            RPS.current = RPS.current < 0 ? 0 : RPS.current;
+        }
 
     }
 
