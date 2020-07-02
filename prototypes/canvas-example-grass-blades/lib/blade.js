1840c2c9ecf7769c09c359dc33a8c1ddaa94e406
dustinpfister <dustin.pfister@gmail.com>
2020-07-02 12:07:29 -0400

    basic create method

--- prototypes/canvas-example-grass-blades/lib/blade.js
+++ prototypes/canvas-example-grass-blades/lib/blade.js
@@ -1,15 +1,28 @@
 
 var Blade = (function () {
 
-
     // public API
     return {
-        // create a state
+        // create a blade
         create: function (opt) {
             opt = opt || {};
-            return {};
+            opt.segCount = opt.seqCount || 10;
+
+            opt.canvas = opt.canvas || {
+                width: 320,
+                heigh: 240
+            };
+            opt.baseX = opt.baseX === undefined ? canvas.width / 2 : opt.baseX;
+            return {
+                basePos: {
+                    y: canvas.height,
+                    x: opt.baseX
+                },
+                segments: [],
+                t: 0,
+                tMax: 3000
+            };
         }
- 
     }
 
 }
