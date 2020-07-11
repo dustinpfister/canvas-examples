83ed8c891f61edf32eb1d4d3ec8c99ff1b746f0e
dustinpfister <dustin.pfister@gmail.com>
2020-07-11 11:37:40 -0400

    have a basic unit.js file

--- forpost/canvas-example-game-fixed-shooter-hybrid/lib/unit.js
+++ forpost/canvas-example-game-fixed-shooter-hybrid/lib/unit.js
@@ -1,22 +1,25 @@
 var Unit = (function () {
 
     var createBaseUnit = function (opt) {
-
         opt = opt || {};
-
         return {
             x: opt.x === undefined ? 0 : opt.x,
             y: opt.y === undefined ? 0 : opt.y,
             w: opt.w === undefined ? 32 : opt.w,
             h: opt.h === undefined ? 32 : opt.h,
+            heading: opt.heading === undefined ? 0 : opt.heading,
+            pps: opt.pps === undefined ? 0 : opt.pps
         };
-
     };
 
     return {
-
-        create: function () {}
-
+        create: createBaseUnit,
+        update: function (unit, secs) {
+            secs = secs === undefined ? 0 : secs;
+            unit.x += Math.cos(unit.heading) * unit.pps * secs;
+            unit.y += Math.sin(unit.heading) * unit.pps * secs;
+        }
     };
 
-});
+}
+    ());
