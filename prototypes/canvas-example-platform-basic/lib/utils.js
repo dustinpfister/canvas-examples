fc97adce908c67542e0a60949ed0b897366d8714
dustinpfister <dustin.pfister@gmail.com>
2020-07-22 09:39:53 -0400

    canvas-example-platfrom-basic prototype

---
+++ prototypes/canvas-example-platform-basic/lib/utils.js
@@ -0,0 +1,19 @@
+// UTILS
+var u = {};
+// bounding box
+utils.bb = function (a, b) {
+    return !(
+        (a.y + a.h) < b.y ||
+        a.y > (b.y + b.h) ||
+        (a.x + a.w) < b.x ||
+        a.x > (b.x + b.w));
+};
+// clamp
+utils.clamp = function (obj, box) {
+    var xMax = box.x + box.w - obj.w,
+    yMax = box.y + box.h - obj.h;
+    obj.x = obj.x > xMax ? xMax : obj.x;
+    obj.y = obj.y > yMax ? yMax : obj.y;
+    obj.x = obj.x < box.x ? box.x : obj.x;
+    obj.y = obj.y < box.y ? box.y : obj.y;
+};
