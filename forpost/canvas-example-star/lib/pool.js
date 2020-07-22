9dd68a949ac04d164ffcde18a6e48606f213466c
dustinpfister <dustin.pfister@gmail.com>
2020-07-22 13:23:28 -0400

    moving stars

--- forpost/canvas-example-star/lib/pool.js
+++ forpost/canvas-example-star/lib/pool.js
@@ -6,6 +6,11 @@ pool.update = function (state, secs) {
     obj;
     while (i--) {
         obj = state.pool[i];
+		
+		// move by heading and pps
+		obj.x += Math.cos(obj.heading) * obj.pps * secs;
+		obj.y += Math.sin(obj.heading) * obj.pps * secs;
+		
         obj.points = starMod.create1({
                 pointCount: obj.pointCount,
                 radius: obj.r1,
@@ -28,6 +33,7 @@ pool.createState = function () {
             r1: 30 + Math.round(20 * Math.random()),
             r2: 10 + Math.round(10 * Math.random()),
             heading: Math.PI * 2 * Math.random(),
+            pps: 32,
             points: []
         });
         pool.update(state, 0);
