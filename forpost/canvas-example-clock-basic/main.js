cef6d8d386204f31a0018ea586336b30108daa2f
dustinpfister <dustin.pfister@gmail.com>
2020-08-05 14:27:04 -0400

    using standard div

--- forpost/canvas-example-clock-basic/main.js
+++ forpost/canvas-example-clock-basic/main.js
@@ -2,7 +2,7 @@
 // create and append canvas element, and get 2d context
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
-container = document.getElementById('gamearea') || document.body;
+container = document.getElementById('canvas-app') || document.body;
 container.appendChild(canvas);
 // set width and height
 canvas.width = 320;
