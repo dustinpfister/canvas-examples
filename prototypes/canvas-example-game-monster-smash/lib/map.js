95f185fa6b8b4ade7d5ff92088bbe0c4f888f977
dustinpfister <dustin.pfister@gmail.com>
2020-08-05 13:25:36 -0400

    created a map.js

---
+++ prototypes/canvas-example-game-monster-smash/lib/map.js
@@ -0,0 +1,36 @@
+// UTILS
+var map = (function () {
+
+    var api = {};
+
+    var createCells = function (map) {
+        var cells = [];
+        var len = map.w * map.h,
+        i = 0;
+        while (i < len) {
+            cells.push({
+                i: i,
+                x: i % map.w,
+                y: Math.floor(i / map.w)
+            });
+            i += 1;
+        }
+        return cells;
+    };
+
+    ap.create = function (opt) {
+
+        opt = opt || {};
+        var map = {
+            w: opt.w || 10,
+            h: opt.h || 8,
+            cells: []
+        };
+        map.cells = createCells(map);
+        return map;
+    }
+
+    return api;
+
+}
+    ());
