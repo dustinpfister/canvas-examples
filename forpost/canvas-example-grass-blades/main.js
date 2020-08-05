f4d3dfefadacd6a28d68ad363437a54dd898f99a
dustinpfister <dustin.pfister@gmail.com>
2020-08-05 15:18:18 -0400

    using standard div for grass blades

--- forpost/canvas-example-grass-blades/main.js
+++ forpost/canvas-example-grass-blades/main.js
@@ -1,29 +1,26 @@
 // MAIN
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
-container = document.getElementById('gamearea') || document.body;
+container = document.getElementById('canvas-app') || document.body;
 container.appendChild(canvas);
 canvas.width = 320;
 canvas.height = 240;
-canvas.style.width = '100%';
-canvas.style.height = '100%';
+//canvas.style.width = '100%';
+//canvas.style.height = '100%';
 ctx.translate(0.5, 0.5);
 
 var g = Grass.create({
         maxBlades: 200,
         canvas: canvas
     });
-
 var lt = new Date();
 var loop = function () {
     var now = new Date(),
     t = now - lt;
     requestAnimationFrame(loop);
     draw.background(ctx, canvas);
-
     Grass.update(g, t);
     draw.grass(ctx, g);
     lt = now;
 };
-
 loop();
