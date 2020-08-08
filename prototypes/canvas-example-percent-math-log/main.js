61bf890e9bd0d4f3ed046336fd5fbdb4e3568c31
dustinpfister <dustin.pfister@gmail.com>
2020-08-08 15:58:04 -0400

    create points method

--- prototypes/canvas-example-percent-math-log/main.js
+++ prototypes/canvas-example-percent-math-log/main.js
@@ -5,6 +5,28 @@ utils.logPer = function (per, high) {
     per = per > 1 ? 1 : per;
     return Math.log((1 + high - 2) + per) / Math.log(high);
 };
+utils.createLogPerPoints = function (high, w, h, len) {
+    high = high === undefined ? 2 : high;
+    w = w === undefined ? 320 : w;
+    h = h === undefined ? 240 : h;
+    len = len === undefined ? 50 : len;
+    var points = [],
+    i = 0,
+    x,
+    y,
+    per;
+    while (i < len) {
+        per = i / len;
+        x = w / (len - 1) * i;
+        y = h - utils.logPer(per, high) * h;
+        points.push({
+            x: x,
+            y: y
+        });
+        i += 1;
+    }
+    return points;
+};
 
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
@@ -16,23 +38,15 @@ ctx.translate(0.5, 0.5);
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-var i = 0,
-x, y, per,
-high = 2,
-len = 50;
+var points = utils.createLogPerPoints(2, canvas.width, canvas.height, 100);
 
-ctx.beginPath();
+ctx.strokeStyle = 'red';
 ctx.lineWidth = 3;
-while (i < len) {
-    per = i / len;
-    x = canvas.width / (len-1) * i;
-    y = canvas.height - utils.logPer(per, high) * canvas.height;
-    if (i === 0) {
-        ctx.moveTo(x, y);
-    } else {
-        ctx.lineTo(x, y);
-    }
+ctx.moveTo(points[0].x, points[0].y);
+var i = 1;
+while (i < points.length) {
+    ctx.lineTo(points[i].x, points[i].y);
     i += 1;
 }
-ctx.strokeStyle = 'red';
 ctx.stroke();
+
