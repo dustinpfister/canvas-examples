1e05862ecddd77a19583166d374d1de1011b337f
dustinpfister <dustin.pfister@gmail.com>
2020-08-13 15:53:11 -0400

    started new state machine object in main.js

--- forpost/canvas-example-game-crosshairs/main.js
+++ forpost/canvas-example-game-crosshairs/main.js
@@ -1,5 +1,6 @@
-// MAIN
+// MAIN file including state machine
 (function () {
+
     var canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d'),
     container = document.getElementById('canvas-app') || document.body;
@@ -8,9 +9,13 @@
     canvas.height = 240;
     ctx.translate(0.5, 0.5);
 
-    var game = gameMod.create({
+    var sm = {
+        canvas: canvas,
+        ctx: ctx,
+        game: gameMod.create({
             canvas: canvas
-        });
+        })
+    };
 
     var lt = new Date(),
     FPS_target = 30;
@@ -20,17 +25,17 @@
         secs = t / 1000;
         requestAnimationFrame(loop);
         if (t >= 1000 / FPS_target) {
-            gameMod.update(game, secs);
+            gameMod.update(sm.game, secs);
             draw.back(ctx, canvas);
-            draw.map(ctx, game.map, game.cross);
-            draw.explosions(ctx, game);
-            draw.cross(ctx, game.cross);
-            draw.shots(ctx, game);
+            draw.map(ctx, sm.game.map, sm.game.cross);
+            draw.explosions(ctx, sm.game);
+            draw.cross(ctx, sm.game.cross);
+            draw.shots(ctx, sm.game);
             draw.buttons(ctx);
-            draw.ver(ctx, game);
-            //draw.statBar(ctx, game);
-            //draw.info(ctx, game);
-            draw.debugAutoPlay(ctx, game);
+            draw.ver(ctx, sm.game);
+            //draw.statBar(ctx, sm.game);
+            //draw.info(ctx, sm.game);
+            draw.debugAutoPlay(ctx, sm.game);
             lt = now;
         }
     };
