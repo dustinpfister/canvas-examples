13295e8aee2e1587473a4b6e929f640084b1f3ce
dustinpfister <dustin.pfister@gmail.com>
2020-08-14 17:01:16 -0400

    using secs argument

--- forpost/canvas-example-pointer-movement/main.js
+++ forpost/canvas-example-pointer-movement/main.js
@@ -15,17 +15,22 @@ var pt = {
     y: 0
 };
 
+var lt = new Date();
 var loop = function () {
+    var now = new Date(),
+    t = now - lt,
+    secs = t / 1000;
     requestAnimationFrame(loop);
     PM.updatePM(pm);
 
-    PM.stepPointByPM(pm, pt, 1);
+    PM.stepPointByPM(pm, pt, secs);
 
     draw.background(pm, ctx, canvas);
     draw.PTGridlines(pt, ctx, canvas);
     draw.navCircle(pm, ctx, canvas);
     draw.debugInfo(pm, pt, ctx, canvas);
     draw.ver(ctx, pm);
+    lt = now;
 };
 loop();
 
