4c0bffe7123fb6a94c561e6cc046b954f6933167
dustinpfister <dustin.pfister@gmail.com>
2020-08-21 09:09:14 -0400

    start a waterfall prototype folder

---
+++ prototypes/canvas-example-waterfall/lib/pool.js
@@ -0,0 +1,76 @@
+var poolMod = (function () {
+
+    return {
+
+        create: function (opt) {
+            opt = opt || {};
+            opt.count = opt.count || 10;
+            var i = 0,
+            pool = [];
+            while (i < opt.count) {
+                pool.push({
+                    active: false,
+                    x: 0,
+                    y: 0,
+                    radius: 8,
+                    heading: 0,
+                    pps: 32,
+                    lifespan: opt.lifespan || 3,
+                    data: {},
+                    spawn: opt.spawn || function (obj, state) {
+                        obj.active = true;
+                    },
+                    purge: opt.purge || function (obj, state) {},
+                    update: opt.update || function (obj, state, secs) {
+                        obj.x += obj.pps * secs;
+                        obj.lifespan -= secs;
+                    }
+                });
+                i += 1;
+            }
+            return pool;
+        },
+
+        spawn: function (pool, game, opt) {
+            var i = pool.length,
+            obj;
+            while (i--) {
+                obj = pool[i];
+                if (!obj.active) {
+                    obj.active = true;
+                    obj.spawn.call(obj, obj, game, opt);
+                    break;
+                }
+            }
+        },
+
+        update: function (pool, state, secs) {
+            var i = pool.length,
+            obj;
+            while (i--) {
+                obj = pool[i];
+                if (obj.active) {
+                    obj.update(obj, state, secs);
+                    obj.lifespan = obj.lifespan < 0 ? 0 : obj.lifespan;
+                    if (obj.lifespan === 0) {
+                        obj.active = false;
+                        obj.purge.call(obj, obj, state);
+                    }
+                }
+            }
+        },
+        // set all to inActive or active state
+        setActiveStateForAll: function (pool, bool) {
+            bool = bool === undefined ? false : bool;
+            var i = pool.length,
+            obj;
+            while (i--) {
+                obj = pool[i];
+                obj.active = bool;
+            }
+        }
+
+    }
+
+}
+    ());
---
+++ prototypes/canvas-example-waterfall/todo.md
@@ -0,0 +1,8 @@
+# todo list for canvas-example-waterfall
+
+## 0.0.0 - Just a basic idea of a waterfall
+* have a pool.js file for a pool of 'water blocks'
+* have water blocks become active at the top right of the canvas
+* water blocks move to the left and then drop down at some point
+* when a water block leaves the canvas have it become inactive again
+* make a pkg_0_0_0.html file
