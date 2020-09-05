d1b0d137da42cff967f4370c94e4c3dc011ee745
dustinpfister <dustin.pfister@gmail.com>
2020-09-05 11:12:04 -0400

    using canvas-app container

--- prototypes/canvas-example-kill-box/main.js
+++ prototypes/canvas-example-kill-box/main.js
@@ -1,12 +1,12 @@
 // MAIN
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
-container = document.getElementById('gamearea') || document.body;
+container = document.getElementById('canvas-app') || document.body;
 container.appendChild(canvas);
 canvas.width = 320;
 canvas.height = 240;
 ctx.translate(0.5, 0.5);
-
+// main app loop
 var loop = function () {
     requestAnimationFrame(loop);
     draw.back(ctx, canvas);
