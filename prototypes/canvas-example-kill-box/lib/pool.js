5bf4be3f1cd2fe8d5ca8facbc0ea67c488e82cb3
dustinpfister <dustin.pfister@gmail.com>
2020-09-05 11:10:46 -0400

    added the pool.js file from chrosshairs

---
+++ prototypes/canvas-example-kill-box/lib/pool.js
@@ -0,0 +1,74 @@
+var poolMod = (function () {
+    return {
+        // create a new pool
+        create: function (opt) {
+            opt = opt || {};
+            opt.count = opt.count || 10;
+            var i = 0,
+            pool = [];
+            while (i < opt.count) {
+                pool.push({
+                    active: false,
+                    x: 0,
+                    y: 0,
+                    radius: 8,
+                    heading: 0,
+                    pps: 32,
+                    lifespan: opt.lifespan || 3,
+                    data: {},
+                    spawn: opt.spawn || function (obj, state) {
+                        obj.active = true;
+                    },
+                    purge: opt.purge || function (obj, state) {},
+                    update: opt.update || function (obj, state, secs) {
+                        obj.x += obj.pps * secs;
+                        obj.lifespan -= secs;
+                    }
+                });
+                i += 1;
+            }
+            return pool;
+        },
+        // spawn the next inactive object in the given pool
+        spawn: function (pool, game, opt) {
+            var i = pool.length,
+            obj;
+            while (i--) {
+                obj = pool[i];
+                if (!obj.active) {
+                    obj.active = true;
+                    obj.spawn.call(obj, obj, game, opt);
+                    break;
+                }
+            }
+        },
+        // update a pool object by a secs value
+        update: function (pool, secs, state) {
+            var i = pool.length,
+            obj;
+            state = state || {}; // your projects state object
+            while (i--) {
+                obj = pool[i];
+                if (obj.active) {
+                    obj.update(obj, state, secs);
+                    obj.lifespan = obj.lifespan < 0 ? 0 : obj.lifespan;
+                    if (obj.lifespan === 0) {
+                        obj.active = false;
+                        obj.purge.call(obj, obj, state);
+                    }
+                }
+            }
+        },
+        // set all to inActive or active state
+        setActiveStateForAll: function (pool, bool) {
+            bool = bool === undefined ? false : bool;
+            var i = pool.length,
+            obj;
+            while (i--) {
+                obj = pool[i];
+                obj.active = bool;
+            }
+        }
+    }
+}
+    ());
