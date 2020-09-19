3725620baa98c4ce5bba4bf221de4c413625de33
dustinpfister <dustin.pfister@gmail.com>
2020-09-19 11:28:59 -0400

    hard coded values in gameMod

--- prototypes/canvas-example-hyper-casual-to-the-black/lib/game.js
+++ prototypes/canvas-example-hyper-casual-to-the-black/lib/game.js
@@ -1,14 +1,37 @@
 var gameMod = (function () {
 
+    var hardCode = {
+       maxDistance: 1000000000000,
+       pps: {
+           start: 32,
+           cap: 2048
+       }
+    };
+
     var api = {};
 
     api.create = function(opt){
         opt = opt || {};
         var game = {
-            distance: opt.distance === undefined ? 0 : opt.distance
+            pps: opt.pps || hardCode.pps.start,
+            distance: opt.distance === undefined ? 0 : opt.distance,
+            target: opt.target === undefined ? hardCode.maxDistance : opt.target
+            timeToTarget: 0
         };
         return game;
     };
+
+var timeToDistance = function(game, distance){
+
+
+
+return 0;
+
+}
+
+api.update = function(game){
+    
+};
 
     return api;
 }
