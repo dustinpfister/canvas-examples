aeee7d9c4c5e381c2268a7eba8e8e301e62fcb80
dustinpfister <dustin.pfister@gmail.com>
2020-11-03 15:29:37 -0500

    game.worldRadius

--- prototypes/canvas-example-game-mr-sun/lib/game.js
+++ prototypes/canvas-example-game-mr-sun/lib/game.js
@@ -6,27 +6,31 @@ var gameMod = (function(){
         opt = opt || {};
         opt.canvas = opt.canvas || {width: 320, height: 240 };
  
+        // base object
         var game = {};
         game.centerX = opt.centerX || opt.canvas.width / 2;
         game.centerY = opt.centerY || opt.canvas.height / 2;
+        game.worldRadius = 100;
+
+        // sun object
         game.sun = {
             radius: 16
         };
         game.sun.x = game.centerX;
         game.sun.y = game.centerY;
+
         // setup sections
         var i = 0,
         sections = [],
         total = 19,
-        radian, 
-        radius = 100,
+        radian,	
         cx = game.centerX,
         cy = game.centerY;
         while(i < total){
             radian = Math.PI * 2 / total * i;
             sections.push({
-                x: Math.cos(radian) * radius + cx,
-                y: Math.sin(radian) * radius + cy,
+                x: Math.cos(radian) * game.worldRadius + cx,
+                y: Math.sin(radian) * game.worldRadius + cy,
                 radius: 16,
                 per: 1
             });
@@ -42,7 +46,7 @@ var gameMod = (function(){
         game.sections.forEach(function(section){
             var ajust = section.radius + sun.radius;
             var d = utils.distance(section.x, section.y, sun.x, sun.y) - ajust;
-            var per = d / (200 - ajust * 2);
+            var per = d / (game.worldRadius * 2 - ajust * 2);
             per = per > 1 ? 1: per;
             per = per < 0 ? 0: per;
             per = 1 - per;
