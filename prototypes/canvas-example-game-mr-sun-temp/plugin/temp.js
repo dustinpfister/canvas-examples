b2881d10d52409275e7f62506982853af10c14e2
dustinpfister <dustin.pfister@gmail.com>
2020-11-04 15:58:01 -0500

    sun temp goes up over time

--- prototypes/canvas-example-game-mr-sun-temp/plugin/temp.js
+++ prototypes/canvas-example-game-mr-sun-temp/plugin/temp.js
@@ -3,8 +3,16 @@ gameMod.load({
     callPriority: '1.0',
     create: function(game, opt){
         console.log(this.name);
+        game.sun.temp = 0;
+        game.sections = game.sections.map((section)=>{
+            section.temp = 0;
+            return section;
+        });
     },
     onDeltaYear: function(game, deltaYears){
-        
+        // sun will gain temp over time
+        game.sun.temp = 10 + 10 * game.year;
+        game.sun.temp = game.sun.temp > 10000 ? 10000: game.sun.temp;
+        console.log('sun temp: ' + game.sun.temp);
     }
 });
