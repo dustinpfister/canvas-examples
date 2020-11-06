e2e762f017f287a93d13d05b0534c01dcfda1152
dustinpfister <dustin.pfister@gmail.com>
2020-11-06 12:31:27 -0500

    fusion of minerals now happens in the sun

--- forpost/canvas-example-game-mr-sun-temp/plugin/fusion.js
+++ forpost/canvas-example-game-mr-sun-temp/plugin/fusion.js
@@ -2,8 +2,8 @@
 gameMod.load((function(){
 
     // helper for creating a delta value for mineral production
-    var getMinDelta = function(section, rate, temp, deltaYears){
-        return rate * Math.floor(section.temp / temp) * deltaYears;
+    var getMinDelta = function(sun, rate, temp, deltaYears){
+        return rate * Math.floor(sun.temp / temp) * deltaYears;
     };
 
     // the plugObj for fusion
@@ -11,34 +11,33 @@ gameMod.load((function(){
         name: 'fusion',
         callPriority: '1.1',
         create: function(game, opt){
-
-            /*
-            game.sections = game.sections.map(function(section){
-                section.minerals = {
-                    copper: 0, 
-                    silver: 0 
-                };
-                return section;
-            });
-            */
             game.sun.minerals = {
                 copper: 0, 
-                silver: 0 
+                iron: 0 
             };
 
         },
         onDeltaYear: function(game, deltaYears){
+            sun = game.sun;
+            if(sun.temp >= 10){
+                sun.minerals.iron += getMinDelta(sun, 1, 10, deltaYears);
+            }
+            if(sun.temp >= 25){
+                sun.minerals.copper += getMinDelta(sun, 0.5, 25, deltaYears);
+            }
+/*
             var i = game.sections.length,
             section;
             while(i--){
                 section = game.sections[i];
-                if(section.temp > 100){
+                if(section.temp > 10){
                     section.minerals.copper += getMinDelta(section, 5, 100, deltaYears);
                 }
-                if(section.temp > 250){
+                if(section.temp > 25){
                     section.minerals.silver += getMinDelta(section, 1, 250, deltaYears);
                 }
             }
+*/
         }
     };
 }()));
