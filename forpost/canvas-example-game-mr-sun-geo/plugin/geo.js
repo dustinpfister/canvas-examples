c9720203bf8532a2f280119d25670d4f8fb14a98
dustinpfister <dustin.pfister@gmail.com>
2020-11-07 10:46:37 -0500

    totalMass property added

--- forpost/canvas-example-game-mr-sun-geo/plugin/geo.js
+++ forpost/canvas-example-game-mr-sun-geo/plugin/geo.js
@@ -1,11 +1,51 @@
 // geo.js plug-in
-gameMod.load({
-    name: 'geo',
-    callPriority: '2',
-    create: function (game, opt) {
-        game.geoData = {
-            totalMass: 0
+gameMod.load((function () {
+
+        var tabulateObjectMass = function (obj) {
+
+            var i = 0,
+            len = Object.keys(obj.minerals).length,
+            minKey,
+            min,
+            mass = 0;
+            while (i < len) {
+                minKey = Object.keys(obj.minerals)[i];
+                min = obj.minerals[minKey];
+                mass += min;
+                i += 1;
+            }
+            return mass;
         };
-    },
-    onDeltaYear: function (game, deltaYears) {}
-});
+
+        var setTotalMass = function (game) {
+            var gd = game.geoData,
+            i = 0,
+            len = game.sections.length,
+            mass,
+            section;
+            gd.totalMass = 0;
+            while (i < len) {
+                section = game.sections[i];
+                section.totalMass = tabulateObjectMass(section);
+                gd.totalMass += section.totalMass;
+                i += 1;
+            }
+        };
+
+        return {
+            name: 'geo',
+            callPriority: '2',
+            create: function (game, opt) {
+                game.geoData = {
+                    totalMass: 0
+                };
+            },
+            onDeltaYear: function (game, deltaYears) {
+
+                setTotalMass(game);
+
+            }
+        };
+
+    }
+        ()));
