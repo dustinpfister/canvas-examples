4a24b5affc8ca09513a2e71fbb1eb946c653aec8
dustinpfister <dustin.pfister@gmail.com>
2020-11-13 15:21:57 -0500

    cleaned up base draw

--- prototypes/canvas-example-game-mr-sun-blocks/states/init-draw-base.js
+++ prototypes/canvas-example-game-mr-sun-blocks/states/init-draw-base.js
@@ -6,22 +6,24 @@ stateMod.load({
             sm.ctx.fillStyle = '#000020';
             sm.ctx.fillRect(0, 0, sm.canvas.width, sm.canvas.height);
         };
+        // basic draw sections method
         sm.draw.sections = function (sm) {
             var ctx = sm.ctx;
             sm.game.sections.forEach(function (section) {
                 var b = 50 + Math.round(section.per * 128);
                 ctx.fillStyle = 'rgb(0,0,' + b + ')';
+                ctx.strokeStyle = '#808080';
                 ctx.beginPath();
                 ctx.arc(section.x, section.y, section.radius, 0, Math.PI * 2);
                 ctx.fill();
+                ctx.stroke();
                 ctx.fillStyle = 'white';
                 ctx.textAlign = 'center';
                 ctx.textBaseline = 'middle';
                 ctx.font = '10px arial';
-                ctx.fillText(section.cookie.count, section.x, section.y);
-                //ctx.fillText(section.energy, section.x, section.y);
             });
         };
+        // draw Mr Sun
         sm.draw.sun = function (sm) {
             var sun = sm.game.sun,
             ctx = sm.ctx;
@@ -30,7 +32,6 @@ stateMod.load({
             ctx.arc(sun.x, sun.y, sun.radius, 0, Math.PI * 2);
             ctx.fill();
             ctx.fillStyle = 'black';
-            ctx.fillText(sm.game.jar.count, sun.x, sun.y);
         };
         // display
         sm.draw.disp = function (sm) {
@@ -40,8 +41,6 @@ stateMod.load({
             ctx.textBaseline = 'top';
             ctx.font = '10px courier';
             ctx.fillText('year: ' + sm.game.year, 3, 10);
-            ctx.fillText('sun exp: ' + sm.game.sun.xp, 3, 20);
-            ctx.fillText('sun level: ' + sm.game.sun.levelObj.level, 3, 30);
         };
         // draw version number
        sm.draw.ver = function (sm) {
