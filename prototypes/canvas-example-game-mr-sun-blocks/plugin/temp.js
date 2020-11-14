4cc054484700d2c4ed94f97c42a5e4d8af4d3b58
dustinpfister <dustin.pfister@gmail.com>
2020-11-13 19:15:35 -0500

    groundTemp cools

--- prototypes/canvas-example-game-mr-sun-blocks/plugin/temp.js
+++ prototypes/canvas-example-game-mr-sun-blocks/plugin/temp.js
@@ -1,14 +1,21 @@
 gameMod.load((function(){
     var MAX_SUN_TEMP = 250,
-    MAX_GROUND_TEMP = 1000;
+    MAX_GROUND_TEMP = 1000,
+    COOL_TIME = 100000;
     // update sun temp
     var updateSun = function(game, deltaYears){
     };
     // update temp for sections
     var updateTempSections = function(game, deltaYears){
+        var timePer = game.year / COOL_TIME;
+        timePer = timePer > 1 ? 1 : timePer;
         game.forSections(function(section){
             section.temp = section.per * MAX_SUN_TEMP;
+            var groundTempLoss = MAX_GROUND_TEMP * 0.75 * timePer;
+            section.groundTemp = MAX_GROUND_TEMP - groundTempLoss;
+
             section.temp = Number(section.temp.toFixed(2));
+            section.groundTemp = Number(section.groundTemp.toFixed(2));
         });
     };
     // plugObj for temp.js
