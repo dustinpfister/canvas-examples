8936ac7b08153f14386c3816aa9416ef57017bc7
dustinpfister <dustin.pfister@gmail.com>
2020-11-23 10:03:34 -0500

    setting up a default sheet

--- forpost/canvas-example-game-mr-sun/state-machine/plugin/core-spritesheets.js
+++ forpost/canvas-example-game-mr-sun/state-machine/plugin/core-spritesheets.js
@@ -3,6 +3,10 @@ stateMod.load({
     type: 'plugin',
     create : function(sm){
 
+        // set up sheets array
+        sm.sheets = [];
+
+        // default code generated sprite sheet
         var defaultSheet = function(){
             var canvas = document.createElement('canvas'),
             ctx = canvas.getContext('2d');
@@ -20,14 +24,30 @@ stateMod.load({
             ctx.moveTo(32, 0);
             ctx.lineTo(0, 32);
             ctx.stroke();
+            return canvas;
         };
 
-        sm.sheets = [];
-        sm.createSpriteSheetObj = function(img, sheetName, frames, index){
+        // use whole image as one frame by default
+        var defaultFrames = function(img){
+            return [ 0, 0, img.width, img.height ];
+        };
+
+        sm.createSpriteSheetObj = function(img, name, frames, index){
             img = img === undefined ? defaultSheet() : img;
             index = index === undefined ? sm.sheets.length : index;
             frames = frames === undefined ? defaultFrames(img) : frames;
-            sheetName = sheetName === undefined ? sm.sheets.length : sheetName;
-        }
+            name = name === undefined ? sm.sheets.length : name;
+            sm.sheets[index] = {
+                name: name,
+                img: img,
+                frames : frames
+            };
+        };
+
+        // set up a 'default' sheet at index 0
+        var img = defaultSheet();
+        sm.createSpriteSheetObj(img, 'default', defaultFrames(img), 0);
+        console.log(sm.sheets);
+
     }
 });
