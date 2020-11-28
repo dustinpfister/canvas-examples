e5433d82ea447140f12ff7f4d2f0f39eee7a1c08
dustinpfister <dustin.pfister@gmail.com>
2020-11-28 14:17:09 -0500

    bounds

--- forpost/canvas-example-grid/lib/grid_cells_draw.js
+++ forpost/canvas-example-grid/lib/grid_cells_draw.js
@@ -1,5 +1,12 @@
 var draw = {};
 
+draw.back = function(ctx, canvas){
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+};
+
 // draw Cell Lines
 draw.cells = function (ctx, grid, style) {
     var ci = 0,
--- forpost/canvas-example-grid/main.js
+++ forpost/canvas-example-grid/main.js
@@ -7,9 +7,25 @@ canvas.height = 240;
 ctx.translate(0.5, 0.5);
 
 var grid = gridMod.create({
-   width: 8,
-   height: 6
+   width: 24,
+   height: 16
 });
 
 
-draw.cells(ctx, grid);
+
+
+// can pull in a loop
+var loop = function () {
+    requestAnimationFrame(loop);
+
+    grid.xOffset -= 1;
+    var xMax = 32;
+    grid.xOffset = grid.xOffset > xMax ? xMax : grid.xOffset;
+    var xMin = xMax + grid.cellSize * grid.width * -1;
+    grid.xOffset = grid.xOffset < xMin ? xMin : grid.xOffset;
+
+    draw.back(ctx, canvas);
+    draw.cells(ctx, grid);
+};
+
+loop();
