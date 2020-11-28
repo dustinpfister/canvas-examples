fb9247855b2bed51a3c7c095a4d1ab0080b3776f
dustinpfister <dustin.pfister@gmail.com>
2020-11-28 13:47:57 -0500

    clean up

--- forpost/canvas-example-grid/lib/grid_cells.js
+++ forpost/canvas-example-grid/lib/grid_cells.js
@@ -1,14 +1,17 @@
 var gridMod = (function(){
-var Grid = function (opt) {
-    opt = opt || {};
-    this.xOffset = opt.xOffset === undefined ? 5 : opt.xOffset;
-    this.yOffset = opt.yOffset === undefined ? 5 : opt.yOffset;
-    this.cellSize = opt.cellSize === undefined ? 32 : opt.cellSize;
-    this.cellWidth = opt.cellWidth || 7;
-    this.cellHeight = opt.cellHeight || 6;
-    // set cells for the grid
-    this.setCells();
-};
+
+
+    var createGrid = function (opt) {
+        opt = opt || {};
+        var grid = {};
+        grid.xOffset = opt.xOffset === undefined ? 5 : opt.xOffset;
+        grid.yOffset = opt.yOffset === undefined ? 5 : opt.yOffset;
+        grid.cellSize = opt.cellSize === undefined ? 32 : opt.cellSize;
+        grid.width = opt.width || 7;
+        grid.height = opt.height || 6;
+        // create cells
+        grid.cells = createBaseCellsArray(grid.width, grid.height);
+    };
 
 
 
--- forpost/canvas-example-grid/todo.md
+++ forpost/canvas-example-grid/todo.md
@@ -1,6 +1,7 @@
 # canvas-example-grid
 
 ## 0.0.0 - A grid_cells.js module
-* start a /lib folder and a grid_cells.js file
+* (done) start a /lib folder and a grid_cells.js file
 * grid_cells.js will be a collection of cell objects
+* create a main.js file that will be used to make a backage for this project.
 * make a pkg-0-1-0.html
