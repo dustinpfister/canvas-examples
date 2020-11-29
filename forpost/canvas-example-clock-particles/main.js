c8d59df1f7e1ce63269fc6036ad2a1e73667cdc9
dustinpfister <dustin.pfister@gmail.com>
2020-11-29 15:05:37 -0500

    using new container div for clock particles

--- forpost/canvas-example-clock-particles/main.js
+++ forpost/canvas-example-clock-particles/main.js
@@ -2,13 +2,13 @@
 // create and append canvas element, and get 2d context
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
-container = document.getElementById('gamearea') || document.body;
+container = document.getElementById('canvas-app') || document.body;
 container.appendChild(canvas);
 // set width and height
 canvas.width = 320;
 canvas.height = 240;
-canvas.style.width = '100%';
-canvas.style.height = '100%';
+//canvas.style.width = '100%';
+//canvas.style.height = '100%';
 
 // loop
 var clock = clockMod.create(new Date());
