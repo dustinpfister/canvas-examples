1f002ee4c9cad9d3ca7f21e168248eb7a154199b
dustinpfister <dustin.pfister@gmail.com>
2020-12-02 16:19:57 -0500

    keyboard movement working

--- prototypes/canvas-example-hyper-casual-space-shooter/main.js
+++ prototypes/canvas-example-hyper-casual-space-shooter/main.js
@@ -16,7 +16,10 @@ var state = {
     ver: '0.1.0',
     canvas : canvasObj.canvas,
     ctx: canvasObj.ctx,
-    game: gameMod.create()
+    game: gameMod.create(),
+    input: {
+        keys: {}
+    }
 };
 
 var lt = new Date(),
@@ -31,8 +34,19 @@ var loop = function () {
     if (t >= FPS_target) {
 
         gameMod.updateMap(game, secs);
-        gameMod.setMapMovement(game, 90, 32);
 
+        if(state.input.keys.a){
+            gameMod.setMapMovement(game, 180, 32);
+        }
+        if(state.input.keys.d){
+            gameMod.setMapMovement(game, 0, 32);
+        }
+        if(state.input.keys.w){
+            gameMod.setMapMovement(game, 90, 32);
+        }
+        if(state.input.keys.s){
+            gameMod.setMapMovement(game, 270, 32);
+        }
         draw.background(state.ctx, state.canvas);
         draw.gridLines(state.ctx, state, 'rgba(255,255,255,0.1)');
         draw.ship(state.ctx, state);
@@ -42,3 +56,15 @@ var loop = function () {
     }
 };
 loop();
+
+// KEYBOARD EVENTS
+window.addEventListener('keydown', function(e){
+    //e.preventDefault();
+    var key = e.key.toLowerCase();
+    state.input.keys[key] = true;
+});
+window.addEventListener('keyup', function(e){
+    //e.preventDefault();
+    var key = e.key.toLowerCase();
+    state.input.keys[key] = false;
+});
