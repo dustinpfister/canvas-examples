7917166fce31df85afa573d9f8e6d1bc3eb4c667
dustinpfister <dustin.pfister@gmail.com>
2020-12-06 09:33:38 -0500

    spawning objects with radian that is at opposite of map

--- prototypes/canvas-example-hyper-casual-space-shooter/lib/game.js
+++ prototypes/canvas-example-hyper-casual-space-shooter/lib/game.js
@@ -11,6 +11,7 @@ var gameMod = (function(){
             },
             blocks: poolMod.create({
                 spawn: function(obj, pool, state, opt){
+                    var game = state.game;
                     //obj.x = state.canvas.width;
                     //console.log(state);
 
@@ -19,8 +20,10 @@ var gameMod = (function(){
                     obj.y = state.game.ship.y + Math.sin(a) * 100;
 
                     //obj.radian = Math.PI * 1; //Math.PI / 180 * 90 * -1;
-        obj.radian = Math.PI / 180 * 90;
-
+                    //obj.radian = Math.PI / 180 * 180;
+ 
+                    obj.radian = game.map.radian + Math.PI;
+                    obj.pps = 16;
 
                     obj.lifespan = 5;
 
@@ -28,14 +31,16 @@ var gameMod = (function(){
                 update: function(obj, pool, state, secs){
                     //obj.lifespan = 1;
                     var map = state.game.map;
-                    var mapDeltaX = Math.cos(map.radian + Math.PI) * (map.pps * 1) * secs;
-                    var mapDeltaY = Math.sin(map.radian) * (map.pps * 1) * secs;
+                    //var mapDeltaX = Math.cos(map.radian) * (map.pps * 1) * secs;
+                    //var mapDeltaY = Math.sin(map.radian) * (map.pps * 1) * secs;
                     var objDeltaX = Math.cos(obj.radian) * obj.pps * secs;
                     var objDeltaY = Math.sin(obj.radian) * obj.pps * secs;
                     //obj.x += mapDeltaX + objDeltaX;
                     //obj.y += mapDeltaY + objDeltaY;
-                    obj.x += mapDeltaX;
-                    obj.y += mapDeltaY;
+                    //obj.x += mapDeltaX;
+                    //obj.y += mapDeltaY;
+                    obj.x += objDeltaX;
+                    obj.y += objDeltaY;
                 }
             }),
             map: { // map position
