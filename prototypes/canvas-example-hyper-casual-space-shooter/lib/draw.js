556dfe3386ba3e4ab436db9ebfddcb927a75b8d5
dustinpfister <dustin.pfister@gmail.com>
2020-12-07 16:49:02 -0500

    drawing a crude yet effective health bar

--- prototypes/canvas-example-hyper-casual-space-shooter/lib/draw.js
+++ prototypes/canvas-example-hyper-casual-space-shooter/lib/draw.js
@@ -13,6 +13,14 @@ var draw = (function(){
             render(ctx, obj);
         }
         if(obj.hp){
+            ctx.beginPath();
+            ctx.rect(obj.x - obj.r, obj.y + obj.r - 5, obj.r * 2, 5);
+            ctx.fillStyle="rgba(120,120,120,0.4)";
+            ctx.fill();
+            ctx.beginPath();
+            ctx.rect(obj.x - obj.r, obj.y + obj.r - 5, obj.r * 2 * obj.hp.per, 5);
+            ctx.fillStyle="rgba(0,255,0,0.4)";
+            ctx.fill();
         }
         ctx.restore();
     };
