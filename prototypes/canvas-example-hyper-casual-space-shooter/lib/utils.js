b02c038550d152114005ac444d6b6278776f00b3
dustinpfister <dustin.pfister@gmail.com>
2020-12-10 20:00:03 -0500

    angleTo utils method

--- prototypes/canvas-example-hyper-casual-space-shooter/lib/utils.js
+++ prototypes/canvas-example-hyper-casual-space-shooter/lib/utils.js
@@ -14,10 +14,16 @@ utils.getCanvasRelative = function (e) {
     };
 };
 
+
+
 utils.distance = function (x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
 
 utils.wrapRadian = function(radian){
     return utils.mod(radian, Math.PI * 2);
+};
+
+utils.angleTo = function(fromX, fromY, toX, toY){
+    return utils.wrapRadian(Math.atan2(toY - fromY, toX - fromX) + Math.PI);
 };
