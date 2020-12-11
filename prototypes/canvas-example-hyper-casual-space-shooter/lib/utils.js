6a24fbdd91d8bc353c9e2d44c79c500a6e2e93c5
dustinpfister <dustin.pfister@gmail.com>
2020-12-11 09:53:30 -0500

    utils.deepClone

--- prototypes/canvas-example-hyper-casual-space-shooter/lib/utils.js
+++ prototypes/canvas-example-hyper-casual-space-shooter/lib/utils.js
@@ -1,9 +1,11 @@
 var utils = {};
 
+// mathematical modulo
 utils.mod = function(x, m) {
     return (x % m + m) % m;
 };
 
+// return a canvas realtive point from the given Mouse or Touch event Object
 utils.getCanvasRelative = function (e) {
     var canvas = e.target,
     bx = canvas.getBoundingClientRect();
@@ -14,16 +16,23 @@ utils.getCanvasRelative = function (e) {
     };
 };
 
-
-
+// get a distance between two points
 utils.distance = function (x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
 
+// Wrap a radian value
 utils.wrapRadian = function(radian){
     return utils.mod(radian, Math.PI * 2);
 };
 
+// get the angle from one point to another
 utils.angleTo = function(fromX, fromY, toX, toY){
     return utils.wrapRadian(Math.atan2(toY - fromY, toX - fromX) + Math.PI);
 };
+
+// deep clone an object
+utils.deepClone = function (obj) {
+    return JSON.parse(JSON.stringify(obj));
+};
+
