94d547a3870f72e1169b50827450cc9d89e5445d
dustinpfister <dustin.pfister@gmail.com>
2020-12-15 10:22:13 -0500

    per-test.js

--- forpost/canvas-example-hyper-casual-space-shooter/main.js
+++ forpost/canvas-example-hyper-casual-space-shooter/main.js
@@ -151,32 +151,35 @@ canvas.addEventListener('mousemove', pointerEvent);
 canvas.addEventListener('mouseup', pointerEvent);
 
 // test - the result of getN should equal the original value of n
-var testForN = function (method, n, d) {
-    var per = method(n, d, 'per'),
-    n2 = method(per, d, 'n');
-    //console.log('n=' + n, 'n2=' + n2, n === n2);
-    //return result;
+var testForN = function (method, n, d, args) {
+    args = args || [];
+    //var per = method(n, d, 'per'),
+    //n2 = method(per, d, 'n');
+    var per = method.apply(null, [n, d, 'per'].concat(args || [])),
+    n2 = method.apply(null,[per, d, 'n'].concat(args || []));
     return {
         method: method,
         n: n,
         d: d,
+        per: per,
         pass: n === n2,
         n2: n2
     }
 };
 
 var n = 0,
-d = 1,
-step = 0.1,
+d = 10,
+step = 1,
 result,
 pass = true,
-method = utils.per;
+method = utils.log3,
+args = [100]
 while (n <= d) {
-    result = testForN(method, n, d);
+    result = testForN(method, n, d, args);
     if (!result.pass) {
         pass = false;
     }
-    console.log('n=' + result.n.toFixed(2), 'n2=' + result.n2.toFixed(2), result.pass);
+    console.log('n=' + result.n.toFixed(2), 'per=' + result.per.toFixed(2), 'n2=' + result.n2.toFixed(2), result.pass);
     n += step;
 }
 console.log('passed? :' + pass);
--- forpost/canvas-example-hyper-casual-space-shooter/per-test.js
+++ forpost/canvas-example-hyper-casual-space-shooter/per-test.js
@@ -1,24 +1,33 @@
 // test - the result of getN should equal the original value of n
-var testForN = function (n, d) {
-    var per = log3.getPer(n, d),
-    n2 = log3.getN(per, d),
-    result = n === n2;
-    console.log(n, n2, n === n2);
-    return result;
+var testForN = function (method, n, d, args) {
+    args = args || [];
+    //var per = method(n, d, 'per'),
+    //n2 = method(per, d, 'n');
+    var per = method.apply(null, [n, d, 'per'].concat(args || [])),
+    n2 = method.apply(null,[per, d, 'n'].concat(args || []));
+    return {
+        method: method,
+        n: n,
+        d: d,
+        per: per,
+        pass: n === n2,
+        n2: n2
+    }
 };
-
-testForN(5, 10);
 
 var n = 0,
 d = 10,
+step = 1,
 result,
-pass = true;
+pass = true,
+method = utils.log3,
+args = [100]
 while (n <= d) {
-    result = testForN(n, d);
-    if (!result) {
+    result = testForN(method, n, d, args);
+    if (!result.pass) {
         pass = false;
     }
-    //console.log( n ,'n == n2 : ' + result );
-    n += 1
+    console.log('n=' + result.n.toFixed(2), 'per=' + result.per.toFixed(2), 'n2=' + result.n2.toFixed(2), result.pass);
+    n += step;
 }
 console.log('passed? :' + pass);
