400789fdc63f346550e8cb6320cdc0f5d0fc1fc0
dustinpfister <dustin.pfister@gmail.com>
2020-12-19 16:13:15 -0500

    build-pkg file

--- forpost/canvas-example-hyper-casual-space-shooter/build/build-pkg.sh
+++ forpost/canvas-example-hyper-casual-space-shooter/build/build-pkg.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 bashdir=$(dirname "${0}")
+pkgfile="${bashdir}/pkg.js"
 pkg=""
 for relpath in $(cat "${bashdir}/files.txt"); do
   echo "${relpath} from ${bashdir}"
   js=$(uglifyjs "${bashdir}/${relpath}")
   pkg="${pkg}${js}"
 done
+
+echo $pkg > ${pkgfile}
--- forpost/canvas-example-hyper-casual-space-shooter/build/build.sh
+++ forpost/canvas-example-hyper-casual-space-shooter/build/build.sh
@@ -4,6 +4,6 @@ bashdir=$(dirname "${0}")
 top=$(cat "${bashdir}/top.txt")
 bottom=$(cat "${bashdir}/bottom.txt")
 js=$(cat "${bashdir}/files.txt" | xargs uglifyjs)
-main=$(uglifyjs "${bashdir}/../main.js")
+#main=$(uglifyjs "${bashdir}/../main.js")
 
 echo "${top}${js}${main}${bottom}"
