5bc143be74609a4672e006ad76871c4c544e76b9
dustinpfister <dustin.pfister@gmail.com>
2020-12-19 18:11:50 -0500

    main build script working

--- bash/build-pkg.sh
+++ bash/build-pkg.sh
@@ -10,8 +10,13 @@ examples=$(ls $dir_for_post)
 for folderName in $examples; do
    dir_build="${dir_for_post}/${folderName}/build"
    build=$( ls $dir_build 2> /dev/null)
-   if [ $? -eq "0" ]
-   then
-      echo $dir_build
+   if [ $? -eq "0" ]; then
+      FILE="${dir_build}/build-pkg.sh"
+      #echo $FILE
+      if [ -f $FILE ]; then
+          echo ""
+          echo "build script found at: $dir_build"
+          ./$FILE
+      fi
    fi
 done
