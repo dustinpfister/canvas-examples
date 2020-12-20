55701558aff0160694142f5d2f92f3e3bbed9d5e
dustinpfister <dustin.pfister@gmail.com>
2020-12-20 09:52:14 -0500

    get-pkg-js.sh script started

--- bash/build-pkg/get-pkg-js.sh
+++ bash/build-pkg/get-pkg-js.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
 # get the pkg javascript
-bashdir=$(dirname "${0}")
+
+# dir of the build folder
+forpost_folder_name="canvas-example-hyper-casual-space-shooter"
+dir_build=$(realpath "../../forpost/${forpost_folder_name}/build")
 pkg=""
-for relpath in $(cat "${bashdir}/files.txt"); do
-  echo "${relpath} from ${bashdir}"
-  js=$(uglifyjs "${bashdir}/${relpath}")
+for relpath in $(cat "${dir_build}/files.txt"); do
+  echo "${relpath} from ${dir_build}"
+  js=$(uglifyjs "${dir_build}/${relpath}")
   pkg="${pkg}${js}"
 done
 
-# create pkg.js
-ver=$(cat "${bashdir}/ver.txt")
-sig=$(cat "${bashdir}/sig.txt")
-pkgfile="${bashdir}/../pkg/${ver}/pkg.js"
-mkdir -p $(dirname $pkgfile)
-echo "Writing pkg file: ${pkgfile}"
-echo -e "$sig\n$pkg" > ${pkgfile}
-
-# cp index.html file to pkg path
-pkgdir=$(dirname $pkgfile)
-echo "Copying index.html"
-cp "${bashdir}/index.html" "${pkgdir}/index.html"
+# $ ./get-pkg-js.sh ../../forpost/canvas-example-hyper-casual-space-shooter/build
