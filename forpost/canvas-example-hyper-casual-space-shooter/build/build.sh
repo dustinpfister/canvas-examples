e48a23df27dea2bef7250f27c4ac2ea3e1634f74
dustinpfister <dustin.pfister@gmail.com>
2020-12-20 16:49:17 -0500

    removed olf files from build folder

--- forpost/canvas-example-hyper-casual-space-shooter/build/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bashdir=$(dirname "${0}")
-top=$(cat "${bashdir}/top.txt")
-bottom=$(cat "${bashdir}/bottom.txt")
-js=$(cat "${bashdir}/files.txt" | xargs uglifyjs)
-#main=$(uglifyjs "${bashdir}/../main.js")
-
-echo "${top}${js}${main}${bottom}"
--- forpost/canvas-example-hyper-casual-space-shooter/build/get-file-list.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bashdir=$(dirname "${0}")
-for i in $(cat "${bashdir}/files.txt"); do
-  uglifyjs "${bashdir}/${i}"
-done
