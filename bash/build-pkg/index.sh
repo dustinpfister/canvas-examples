c8b1e1a3dfe3e529792dd678c16ebd19c1f1d789
dustinpfister <dustin.pfister@gmail.com>
2020-12-20 14:52:35 -0500

    index.sh and pkg-make-all.sh looking good

--- bash/build-pkg/index.sh
+++ bash/build-pkg/index.sh
@@ -2,21 +2,5 @@
 
 # dirs
 dir_this_script=$(dirname $( realpath $0 ) )
-dir_ce_root=$( realpath "${dir_this_script}/../.." )
-dir_for_post=$( realpath "${dir_ce_root}/forpost" )
 
-# loop examples folders
-examples=$(ls $dir_for_post)
-for folderName in $examples; do
-   dir_build="${dir_for_post}/${folderName}/build"
-   build=$( ls $dir_build 2> /dev/null)
-   if [ $? -eq "0" ]; then
-      FILE="${dir_build}/build-pkg.sh"
-      #echo $FILE
-      if [ -f $FILE ]; then
-          echo ""
-          echo "build script found at: $dir_build"
-          ./$FILE
-      fi
-   fi
-done
+${dir_this_script}/pkg-make-all.sh
--- bash/build-pkg/pkg-make-all.sh
+++ bash/build-pkg/pkg-make-all.sh
@@ -8,16 +8,5 @@ dir_for_post=$( realpath "${dir_ce_root}/forpost" )
 # loop examples folders
 examples=$(ls $dir_for_post)
 for folderName in $examples; do
-  ./pkg-make.sh  $folderName
-#   dir_build="${dir_for_post}/${folderName}/build"
-#   build=$( ls $dir_build 2> /dev/null)
-#   if [ $? -eq "0" ]; then
-#      FILE="${dir_build}/build-pkg.sh"
-#      #echo $FILE
-#      if [ -f $FILE ]; then
-#          echo ""
-#          echo "build script found at: $dir_build"
-#          ./$FILE
-#      fi
-#   fi
+  ${dir_this_script}/pkg-make.sh  $folderName
 done
