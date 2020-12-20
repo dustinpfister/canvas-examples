36ef4ea3572d52243496cb044bfd430f475cc8d7
dustinpfister <dustin.pfister@gmail.com>
2020-12-20 09:57:03 -0500

    realpath used to get paths to files

--- bash/build-pkg/get-pkg-js.sh
+++ bash/build-pkg/get-pkg-js.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 # get the pkg javascript
+# $ ./get-pkg-js.sh canvas-example-hyper-casual-space-shooter
 
 # dir of the build folder
-forpost_folder_name="canvas-example-hyper-casual-space-shooter"
-dir_build=$(realpath "../../forpost/${forpost_folder_name}/build")
+forpost_folder_name=$1  # "canvas-example-hyper-casual-space-shooter"
+dir_build=$( realpath "../../forpost/${forpost_folder_name}/build" )
+
+# the pkg var that will hold the finished package
 pkg=""
-for relpath in $(cat "${dir_build}/files.txt"); do
-  echo "${relpath} from ${dir_build}"
-  js=$(uglifyjs "${dir_build}/${relpath}")
+
+for relpath in $( cat "${dir_build}/files.txt" ); do
+  dir_filepath=$( realpath "${dir_build}/${relpath}" )
+  echo "${dir_filepath}"
+  js=$( uglifyjs ${dir_filepath} )
   pkg="${pkg}${js}"
 done
 
-# $ ./get-pkg-js.sh ../../forpost/canvas-example-hyper-casual-space-shooter/build
