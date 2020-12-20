48eb96cbec379c1fd49bce1d882d9382f88e8404
dustinpfister <dustin.pfister@gmail.com>
2020-12-20 13:31:40 -0500

    paths

--- bash/paths.sh
+++ bash/paths.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # simple script to get paths
 
-dir_this_script=$(dirname $0)
+dir_this_script=$(dirname $(realpath $_) )
 dir_ce_root=$(dirname "../${0}");
 dir_for_post="${dir_ce_root}/forpost"
 
 echo $dir_this_script
 echo $dir_ce_root
-ls $dir_for_post
+echo $dir_for_post
