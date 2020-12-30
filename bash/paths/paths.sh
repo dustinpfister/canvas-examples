b4a20e12d17f5fdbc874a967cb17e5980065346a
dustinpfister <dustin.pfister@gmail.com>
2020-12-30 08:46:44 -0500

    comments in paths.sh

--- bash/paths/paths.sh
+++ bash/paths/paths.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-# simple script to get paths
+# This is a simple script to get paths that is absolute paths to the location of this 
+# script as well as other locations of interest. These will need to be ajusted if you 
+# move the script to another location
 
-# these will need to be ajusted if you move the script to nother location
+# this should resolve to the location of THIS SCRIPT
 dir_this_script=$(dirname $( realpath $0 ) )
+
+# this should resolve to the location of the canvas-examples root
+# name space
 dir_ce_root=$( realpath "${dir_this_script}/../.." )
+
+# this should resolve to the forpost folder
 dir_for_post=$( realpath "${dir_ce_root}/forpost" )
 
+# echo the results
 echo $dir_this_script
 echo $dir_ce_root
 echo $dir_for_post
