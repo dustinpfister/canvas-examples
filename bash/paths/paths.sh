e76de9ce7193c5fa841bd349b6d7f36f1031f9c1
dustinpfister <dustin.pfister@gmail.com>
2020-12-30 09:11:29 -0500

    updated paths.js to create a dir_home and dir_js_target paths

--- bash/paths/paths.sh
+++ bash/paths/paths.sh
@@ -4,7 +4,7 @@
 # move the script to another location
 
 # this should resolve to the location of THIS SCRIPT
-dir_this_script=$(dirname $( realpath $0 ) )
+dir_this_script=$( dirname $( realpath $0 ) )
 
 # this should resolve to the location of the canvas-examples root
 # name space
@@ -14,9 +14,12 @@ dir_ce_root=$( realpath "${dir_this_script}/../.." )
 dir_for_post=$( realpath "${dir_ce_root}/forpost" )
 
 # default target for a /js folder for the examples to be used in production
-dir_js_target="~/js/canvas-examples/"
+dir_home=$( realpath ~ )
+dir_js_target="${dir_home}/js/canvas-examples"
 
 # echo the results
 echo $dir_this_script
 echo $dir_ce_root
 echo $dir_for_post
+echo $dir_home
+echo $dir_js_target
