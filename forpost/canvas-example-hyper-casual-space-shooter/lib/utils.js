0827914c0b1c808e91aca6d29dee97dd2117b26e
dustinpfister <dustin.pfister@gmail.com>
2021-01-02 11:59:25 -0500

    format number function

--- forpost/canvas-example-hyper-casual-space-shooter/lib/utils.js
+++ forpost/canvas-example-hyper-casual-space-shooter/lib/utils.js
@@ -1,5 +1,18 @@
 var utils = {};
 
+// format number as money
+// https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+utils.format_money = function(number){
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
+    return formatter.format(number); /* $2,500.00 */
+};
+
 // mathematical modulo
 utils.mod = function(x, m) {
     return (x % m + m) % m;
