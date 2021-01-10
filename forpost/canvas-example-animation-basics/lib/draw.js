406209eac7f89607808d96fa73921ba831660a4b
dustinpfister <dustin.pfister@gmail.com>
2021-01-10 15:56:16 -0500

    draw.js

---
+++ forpost/canvas-example-animation-basics/lib/draw.js
@@ -0,0 +1,34 @@
+var draw = {};
+draw.background = function(ctx, canvas){
+    ctx.fillStyle='black';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+};
+draw.ver = function(ctx, canvas, state){
+    ctx.fillStyle='white';
+    ctx.textBaseline='top';
+    ctx.textAlign='left';
+    ctx.font='10px arial';
+    ctx.fillText('v' + state.ver, 2, canvas.height - 12);
+};
+draw.box = function(ctx, box){
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.rect(box.x, box.y, box.w, box.h);
+    ctx.fill();
+};
+draw.box2 = function(ctx, box){
+    ctx.save();
+    ctx.fillStyle = box.fillStyle || 'red';
+    ctx.strokeStyle = box.strokeStyle || 'white';
+    ctx.beginPath();
+    ctx.translate(box.x, box.y);
+    ctx.rotate(box.r);
+    ctx.rect(box.w / 2 * -1, box.h / 2 * -1, box.w, box.h);
+    if(box.fillStyle){
+        ctx.fill();
+    }
+    if(box.strokeStyle){
+        ctx.stroke();
+    }
+    ctx.restore();
+};
