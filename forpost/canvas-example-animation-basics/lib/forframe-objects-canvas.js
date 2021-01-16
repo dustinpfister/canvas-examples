dceeb5e73d1787a40719ebe8827532970fd94078
dustinpfister <dustin.pfister@gmail.com>
2021-01-16 17:35:55 -0500

    drawing an array of boxes for boxGroup example

--- forpost/canvas-example-animation-basics/lib/forframe-objects-canvas.js
+++ forpost/canvas-example-animation-basics/lib/forframe-objects-canvas.js
@@ -38,18 +38,20 @@ var canvasObjects = (function(){
             forFrame: function(ff, model, frame, maxFrame, per){
                 var size = 8,
                 halfSize = size / 2;
-                return {
+                return [{
                     x: (ff.width - size) * ff.per,
                     y: ff.height / 2 - halfSize,
                     w: size,
                     h: size
-                };
+                }];
             }
         }),
         draw: function(ff, ctx, canvas){
             var box = ff.model;
             ctx.fillStyle='white';
-            ctx.fillRect(box.x, box.y, box.w, box.h);
+            ff.model.forEach(function(box){
+                ctx.fillRect(box.x, box.y, box.w, box.h);
+            });
         },
         backFill: false
     };
