3d25efe1a8f4345e2ec8301ffaff0d8b5bfb043c
dustinpfister <dustin.pfister@gmail.com>
2021-01-15 19:34:00 -0500

    cleaner look

--- forpost/canvas-example-animation-basics/lib/forframe-objects-canvas.js
+++ forpost/canvas-example-animation-basics/lib/forframe-objects-canvas.js
@@ -1,37 +1,33 @@
 
 var canvasObjects = (function(){
 
-
-    var ffDraw = function(ff, ctx, canvas){
-        ctx.fillStyle = 'white';
-        ctx.textBaseline = 'top';
-        //ctx.fillText(ff.frame, 5, 5);
-        var box = ff.model;
-        ctx.fillRect(box.x, box.y, box.w, box.h);
-    };
-
-    var objects = {
-        box: {
-            ff: forFrame.create({
-                type: 'points',
-                maxFrame: 30,
-                width: 64,
-                height: 64,
-                forFrame: function(ff, model, frame, maxFrame, per){
-                    return {
-                        x: (ff.width - 8) * ff.per,
-                        y: ff.height / 2 - 4,
-                        w: 8,
-                        h: 8
-                    };
-                }
-            })
+    var objects = {}
+    objects.box = {
+        ff: forFrame.create({
+            type: 'points',
+            maxFrame: 30,
+            width: 64,
+            height: 64,
+            forFrame: function(ff, model, frame, maxFrame, per){
+                return {
+                    x: (ff.width - 8) * ff.per,
+                    y: ff.height / 2 - 4,
+                    w: 8,
+                    h: 8
+                };
+            }
+        }),
+        draw: function(ff, ctx, canvas){
+            var box = ff.model;
+            ctx.fillStyle='white';
+            ctx.fillRect(box.x, box.y, box.w, box.h);
         }
     };
+   
 
 
     return function(){
-        return forFrame.createCanvas(objects.box.ff, ffDraw, '#004f00');
+        return forFrame.createCanvas(objects.box.ff,objects.box.draw, '#004f00');
     };
 
 }());
