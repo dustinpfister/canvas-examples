b698ca354a01aaba3ed7a0b79b124014083caa6b
dustinpfister <dustin.pfister@gmail.com>
2021-01-17 14:46:04 -0500

    get-forpost-list is creating a basic list

--- node/server/middleware/get-forpost-list.js
+++ node/server/middleware/get-forpost-list.js
@@ -1,5 +1,12 @@
+const fs = require('fs');
 
 module.exports = (req, res, next) => {
-    res.for_post_list = ['foo'];
-    next();
+
+    res.for_post_list = [];
+    fs.readdir(req.app.get('dir_forpost'), (e, files) => {
+        if(files){
+            res.for_post_list = files;
+        }
+        next();
+    });
 };
