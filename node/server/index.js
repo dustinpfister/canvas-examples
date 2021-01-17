f9ea95489ba5dcab557a01480aca7ca9ffd3563a
dustinpfister <dustin.pfister@gmail.com>
2021-01-17 14:30:05 -0500

    dir_forpost

--- node/server/index.js
+++ node/server/index.js
@@ -6,7 +6,8 @@ let app = express();
 // set paths
 app.set('dir_server', __dirname);
 app.set('dir_views', path.join(__dirname, 'views'));
-app.set('dir_root', path.resolve(__dirname, '../..'));
+app.set('dir_root', path.resolve(__dirname, '../..') );
+app.set('dir_forpost', path.resolve(app.get('dir_root'), 'forpost'));
 
 // view is ejs
 app.set('views', app.get('dir_views'));
@@ -14,7 +15,8 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
     res.render('index', {
-        dir_root: app.get('dir_root')
+        dir_root: app.get('dir_root'),
+        dir_forpost: app.get('dir_forpost')
     });
 });
 
