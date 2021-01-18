da68477f8eaa87449eba1d38d4db6bb56cd13b85
dustinpfister <dustin.pfister@gmail.com>
2021-01-18 09:23:21 -0500

    clean up

--- node/server/middleware/get-current-data.js
+++ node/server/middleware/get-current-data.js
@@ -22,14 +22,5 @@ module.exports = (req, res, next) => {
 
         next();
     });
-/*
-    res.for_post_list = [];
-    fs.readdir(req.app.get('dir_forpost'), (e, files) => {
-        if(files){
-            res.for_post_list = files;
-        }
-        next();
-    });
-*/
 
 };
