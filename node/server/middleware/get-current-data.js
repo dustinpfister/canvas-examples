5e3bcdc495813bec2454ec103c6afa1dd198a847
dustinpfister <dustin.pfister@gmail.com>
2021-01-18 10:05:35 -0500

    parseJSURLS helper

--- node/server/middleware/get-current-data.js
+++ node/server/middleware/get-current-data.js
@@ -2,6 +2,12 @@ const fs = require('fs'),
 path = require('path'),
 url = require('url');
 
+var parseJSURLS = (req, files) => {
+    return files.toString().split('\n').map((relPath)=>{
+        return url.resolve(req.protocol + '://' + req.headers.host +'/js/' + req.params.exampleName + '/build/', relPath);
+    });
+};
+
 module.exports = (req, res, next) => {
 
     var dir_canvas_example = path.join( req.app.get('dir_forpost'), req.params.exampleName),
@@ -17,9 +23,7 @@ module.exports = (req, res, next) => {
         }else{
             res.current = {
                 success: true,
-                javaScriptFiles: files.toString().split('\n').map((relPath)=>{
-                    return url.resolve(req.protocol + '://' + req.headers.host +'/js/' + exampleName + '/build/', relPath);
-                })
+                javaScriptFiles: parseJSURLS(req, files)
             };
         }
 
