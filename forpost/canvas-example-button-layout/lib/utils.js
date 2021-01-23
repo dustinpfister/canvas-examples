db66a8a20cd976c4e6428bc736577b61dba114ce
dustinpfister <dustin.pfister@gmail.com>
2021-01-23 15:56:11 -0500

    better createCanvas utils method

--- forpost/canvas-example-button-layout/lib/utils.js
+++ forpost/canvas-example-button-layout/lib/utils.js
@@ -26,8 +26,13 @@ utils.mkButtonLayout = function (opt) {
                 break;
             }
         }
+        return false;
     };
-    blObj.attachTo.addEventListener('click', blObj.handler);
+    blObj.attachTo.addEventListener('click', function(e){
+         e.preventDefault();
+         blObj.handler(e);
+         return false;
+    });
     return blObj;
 };
 
@@ -36,12 +41,18 @@ utils.createCanvas = function(opt){
     opt = opt || {};
     opt.container = opt.container || document.getElementById('canvas-app') || document.body;
     opt.canvas = document.createElement('canvas');
-    opt.canvas.className = 'canvas_example';
     opt.ctx = opt.canvas.getContext('2d');
-    opt.container.appendChild(opt.canvas);
+    // assign the 'canvas_example' className
+    opt.canvas.className = 'canvas_example';
+    // set native width
     opt.canvas.width = opt.width === undefined ? 320 : opt.width;
     opt.canvas.height = opt.height === undefined ? 240 : opt.height;
+    // translate by 0.5, 0.5
     opt.ctx.translate(0.5, 0.5);
+    // disable default action for onselectstart
+    opt.canvas.onselectstart = function () { return false; }
+    // append canvas to container
+    opt.container.appendChild(opt.canvas);
     return opt;
 };
 
