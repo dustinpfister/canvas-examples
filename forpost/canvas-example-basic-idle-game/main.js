22f7562bfdf4412d9ea972c616e49cd8bdccfe97
dustinpfister <dustin.pfister@gmail.com>
2021-01-25 14:36:13 -0500

    using new create canvas method for basic idle game example

--- forpost/canvas-example-basic-idle-game/main.js
+++ forpost/canvas-example-basic-idle-game/main.js
@@ -1,10 +1,17 @@
 // create and append canvas element, and get 2d context
+/*
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
 container = document.getElementById('canvas-app') || document.body;
 container.appendChild(canvas);
 canvas.width = 320;
 canvas.height = 240;
+*/
+
+var canvasObj = utils.createCanvas(),
+canvas = canvasObj.canvas
+ctx = canvasObj.ctx;
+
 
 // create state
 var state = game.getState();
