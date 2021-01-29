787c2ead6cab4c1edc8633a48e6f5e23935b6770
dustinpfister <dustin.pfister@gmail.com>
2021-01-29 12:29:26 -0500

    removed old code from main and draw

--- prototypes/canvas-example-animation-pixmaps/main.js
+++ prototypes/canvas-example-animation-pixmaps/main.js
@@ -10,33 +10,10 @@ var state = {
     canvas: canvasObj.canvas,
     ctx: canvasObj.ctx,
     lt: new Date(),
-    sheets:{
-        tri: canvasObjects('tri'),
-        boxGroup: canvasObjects('boxGroup'),
-        gridLines: canvasObjects('gridLines')
-    },
-    ships: {},
     framesPerSec: 20,
     secs: 0
 };
-// 'ship' objects
-state.ships = poolMod.create({
-    count: 100,
-    spawn: function(obj, pool, state, opt){
-        obj.x = state.canvas.width / 2;
-        obj.y = state.canvas.height / 2;
-        obj.heading = utils.pi2 * Math.random();
-        obj.pps = 16 + 128 * Math.random();
-        obj.lifespan = LIFESPAN;
-        var keys = ['tri', 'boxGroup']; //Object.keys(state.sheets);
-        obj.sheetName = keys[ Math.floor(Math.random() * keys.length) ];
-    },
-    update: function(obj, pool, state, secs){
-        // have a frame index value to use with the sheet to skin the object
-        obj.frameIndex = Math.floor(state.sheets.tri.maxFrame * ( obj.heading / utils.pi2 ));
-        obj.heading += utils.pi2 / 360 * 90 * secs;
-    }
-});
+
 
 // basic app loop
 var loop = function(){
@@ -46,19 +23,8 @@ var loop = function(){
     state.secs += secs;
     if(state.secs >= 1 / state.framesPerSec){
         // draw
-        draw.background(state.ctx, state.canvas, state.sheets.gridLines);
-        var ships = poolMod.getAllActive(state.ships, true);
-        ships.forEach(function(ship){
-            state.ctx.globalAlpha = ship.lifespan / LIFESPAN;
-            state.sheets[ship.sheetName].set(ship.frameIndex);
-            state.sheets[ship.sheetName].draw(state.ctx, ship.x - 16, ship.y - 16, ship.w, ship.h);
-        });
-        state.ctx.globalAlpha = 1;
+        draw.background(state.ctx, state.canvas);
         draw.ver(state.ctx, state.canvas, state);
-        // update
-        poolMod.spawn(state.ships, state, {});
-        poolMod.update(state.ships, state.secs, state);
-        state.sheets.gridLines.step();
         state.secs = 0;
     }
     state.lt = now;
