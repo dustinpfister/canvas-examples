f28914bdd961c45dbeb407bf81096dbb8afa5b52
dustinpfister <dustin.pfister@gmail.com>
2021-01-29 11:08:04 -0500

    build folder for game-map-idle

--- forpost/canvas-example-game-map-idle/main.js
+++ forpost/canvas-example-game-map-idle/main.js
@@ -2,7 +2,7 @@
 // CANVAS
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
-container = document.getElementById('gamearea') || document.body;
+container = document.getElementById('canvas-app') || document.body;
 container.appendChild(canvas);
 canvas.width = 320;
 canvas.height = 240;
