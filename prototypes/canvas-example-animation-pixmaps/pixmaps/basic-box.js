7421373202bef6417e3f748b77cd0c991622306c
dustinpfister <dustin.pfister@gmail.com>
2021-01-29 12:37:43 -0500

    started a pixmaps folder

---
+++ prototypes/canvas-example-animation-pixmaps/pixmaps/basic-box.js
@@ -0,0 +1,24 @@
+pixmapMod.load({
+    name: 'box-basics',  // animation set name
+    palettes: [
+        [false, 'black']
+    ],
+    ani:{ // the collection of animations
+        box1: {  // the first animation called 'box1'
+            paletteIndex: 0,
+            w: 4,
+            h: 4,
+            data: [
+                1,1,1,1, // frame 0
+                1,0,0,1,
+                1,0,0,1,
+                1,1,1,1,
+ 
+                0,0,0,0, // frame 1
+                0,1,1,0,
+                0,1,1,0,
+                0,0,0,0,
+            ]
+        }
+    }
+});
