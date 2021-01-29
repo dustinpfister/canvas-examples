a7bf3478c1307827c365c424a567d54cbc44ebe7
dustinpfister <dustin.pfister@gmail.com>
2021-01-29 12:42:09 -0500

    load function started

--- prototypes/canvas-example-animation-pixmaps/lib/pixmaps.js
+++ prototypes/canvas-example-animation-pixmaps/lib/pixmaps.js
@@ -2,6 +2,15 @@ var pixmapMod = (function(){
 
     var api = {};
 
+    var pixmaps = {};
+
+    api.load = function(pixObj){
+        var pixKey = pixObj.name || 'pix_' + Object.keys(pixmaps).length;
+        // just ref it in for now as long as that works okay
+        pixmaps[pixKey] = pixObj;
+        console.log(pixmaps);
+    };
+
     api.create = function(opt){
     };
 
