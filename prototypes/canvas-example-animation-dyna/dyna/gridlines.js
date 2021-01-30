12697bba6c1b10f54d3a80443a242e4a1b8eee1e
dustinpfister <dustin.pfister@gmail.com>
2021-01-30 12:36:02 -0500

    started a dyna folder

---
+++ prototypes/canvas-example-animation-dyna/dyna/gridlines.js
@@ -0,0 +1,35 @@
+dynaMod.push({
+    // the ff object
+    ff: forFrame.create({
+        maxFrame: 16,
+        width: 256,
+        height: 256,
+        forFrame: function(ff, options){
+            var lines = [],
+            size = ff.width / options.divs,
+            xOff = (size * ff.per) %  size;
+            var i = 0;
+            while(i < options.divs){
+                lines.push(
+                    {sx: xOff + size * i, sy: 0, ex:  xOff + size * i, ey: ff.height},
+                    {sx: 0, sy: size * i, ex: ff.width, ey: size * i}
+                );
+                i += 1
+            }
+            lines.push({sx: 0, sy: ff.height, ex: ff.width, ey: ff.height});
+            return lines;
+        }
+    }),
+    // the draw method
+    draw: function(ff, ctx, canvas){
+        var box = ff.model;
+        ctx.strokeStyle='rgba(255,255,255,0.2)';
+        ctx.lineWidth = 3;
+        ff.model.forEach(function(line){
+            ctx.beginPath();
+            ctx.moveTo(line.sx, line.sy)
+            ctx.lineTo(line.ex, line.ey);
+            ctx.stroke();
+        });
+    }
+});
--- prototypes/canvas-example-animation-dyna/todo.md
+++ prototypes/canvas-example-animation-dyna/todo.md
@@ -1,4 +1,9 @@
 
 ## 0.0.0 - basic idea started
 * (done) start with a copy of the source code for canvas-animation-pixmaps 0.3.0
+* (done) remove all code that has to do with pixmaps, but keep the new, clean forFrame.js and more
 
+* start a dyna.js file
+* start a /dyna/gridlines.js file
+
+* make a pkg 0.0.0 folder
