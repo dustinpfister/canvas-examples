dc168e5161ca2bd00702616ca4f032b0540d9ea1
dustinpfister <dustin.pfister@gmail.com>
2021-02-16 17:11:44 -0500

    build folder for monster smash

--- forpost/canvas-example-game-monster-smash/lib/utils.js
+++ forpost/canvas-example-game-monster-smash/lib/utils.js
@@ -20,13 +20,39 @@ utils.angleToPoint = function (x1, y1, x2, y2, scale) {
     var aTan = Math.atan2(y1 - y2, x1 - x2);
     return (aTan + Math.PI) / (Math.PI * 2) * scale;
 };
-// get a point relative to a canvas element rather than window
+// create a canvas element
+utils.createCanvas = function(opt){
+    opt = opt || {};
+    opt.container = opt.container || document.getElementById('canvas-app') || document.body;
+    opt.canvas = document.createElement('canvas');
+    opt.ctx = opt.canvas.getContext('2d');
+    // assign the 'canvas_example' className
+    opt.canvas.className = 'canvas_example';
+    // set native width
+    opt.canvas.width = opt.width === undefined ? 320 : opt.width;
+    opt.canvas.height = opt.height === undefined ? 240 : opt.height;
+    // translate by 0.5, 0.5
+    opt.ctx.translate(0.5, 0.5);
+    // disable default action for onselectstart
+    opt.canvas.onselectstart = function () { return false; }
+    // append canvas to container
+    opt.container.appendChild(opt.canvas);
+    return opt;
+};
+// get a point relative to the canvas rather than window, 
+// and scale the point to fit the current scaled size of the canvas
 utils.getCanvasRelative = function (e) {
     var canvas = e.target,
-    bx = canvas.getBoundingClientRect();
-    return {
+    bx = canvas.getBoundingClientRect(),
+    pos = {
         x: (e.changedTouches ? e.changedTouches[0].clientX : e.clientX) - bx.left,
         y: (e.changedTouches ? e.changedTouches[0].clientY : e.clientY) - bx.top,
         bx: bx
     };
+    // ajust for native canvas matrix size
+    pos.x = Math.floor((pos.x / canvas.scrollWidth) * canvas.width);
+    pos.y = Math.floor((pos.y / canvas.scrollHeight) * canvas.height);
+    // prevent default
+    e.preventDefault();
+    return pos;
 };
