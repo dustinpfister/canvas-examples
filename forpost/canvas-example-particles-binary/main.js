db6bc8dc06eeb862a5d2f5f0a8ccb76eff40e564
dustinpfister <dustin.pfister@gmail.com>
2021-02-17 08:01:33 -0500

    using create canvas method

--- forpost/canvas-example-particles-binary/main.js
+++ forpost/canvas-example-particles-binary/main.js
@@ -1,14 +1,7 @@
 // MAIN
-var canvas = document.createElement('canvas'),
-ctx = canvas.getContext('2d'),
-container = document.getElementById('gamearea') || document.body;
-container.appendChild(canvas);
-canvas.width = 320;
-canvas.height = 240;
-canvas.style.width = '100%';
-canvas.style.height = '100%';
-ctx.translate(0.5, 0.5);
-
+var canvasObj = u.createCanvas(),
+canvas = canvasObj.canvas,
+ctx = canvasObj.ctx;
 var state = paricles.create({
         canvas: canvas,
         ctx: ctx
@@ -19,7 +12,5 @@ var loop = function () {
     draw.background(state);
     draw.pool(state);
     paricles.update(state);
-
 };
-
 loop();
