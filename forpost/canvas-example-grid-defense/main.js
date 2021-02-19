6e44a6f87539abe91e7598185f23dfef1427972c
dustinpfister <dustin.pfister@gmail.com>
2021-02-19 12:23:44 -0500

    main.js clean up

--- forpost/canvas-example-grid-defense/main.js
+++ forpost/canvas-example-grid-defense/main.js
@@ -14,24 +14,8 @@
     var canvas = canvasObj.canvas;
     var ctx = canvasObj.ctx;
 
-/*	
-    // create and append canvas element, and get 2d context
-    var canvas = document.createElement('canvas'),
-    ctx = canvas.getContext('2d'),
-    container = document.getElementById('gamearea') || document.body;
-    container.appendChild(canvas);
-    // set width and height
-    canvas.width = 320;
-    canvas.height = 240;
-*/
     // single event handler
     canvas.addEventListener('click', function (e) {
-/*
-        var bx = e.target.getBoundingClientRect(),
-        x = e.clientX - bx.left,
-        y = e.clientY - bx.top;
-*/
-
         var pos = utils.getCanvasRelative(e);
         var cell = state.grid.getCellFromPoint(pos.x, pos.y);
         if (cell.enemy) {
