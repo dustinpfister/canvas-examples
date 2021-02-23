5879c90555eee6c450c5e14b691facccaafb3e71
dustinpfister <dustin.pfister@gmail.com>
2021-02-23 16:25:48 -0500

    build folder for image-limits

--- forpost/canvas-example-image-limits/main.js
+++ forpost/canvas-example-image-limits/main.js
@@ -1,9 +1,23 @@
+/*
 var canvas = document.getElementById('the-canvas'),
 ctx = canvas.getContext('2d'),
 inputStr = document.getElementById('img-str');
 
 canvas.width = 320;
 canvas.height = 320;
+*/
+
+var canvasObj = utils.createCanvas(),
+canvas = canvasObj.canvas,
+ctx = canvasObj.ctx;
+container = canvasObj.container;
+
+var inputStr = document.createElement('input');
+inputStr.setAttribute('id', 'img-str');
+inputStr.setAttribute('type', 'text');
+canvasObj.container.appendChild(inputStr);
+
+//var inputStr = document.getElementById('img-str');
 
 var w = 4, h = 4,
 colorDepth = 2,
@@ -14,10 +28,16 @@ IMG.draw(canvas, matrix);
 
 // update by clicking canvas
 canvas.addEventListener('click', function (e) {
+/*
     var bx = e.target.getBoundingClientRect(),
     size = canvas.width / w,
     x = Math.floor((e.clientX - bx.left) / size),
     y = Math.floor((e.clientY - bx.top) / size),
+*/
+    var pos = utils.getCanvasRelative(e),
+    size = canvas.width / w,
+    x = Math.floor(pos.x / size),
+    y = Math.floor(pos.y / size),
     px = matrix[y][x];
     px += 1;
     if (px >= colorDepth) {
@@ -27,6 +47,11 @@ canvas.addEventListener('click', function (e) {
     str = IMG.chunkToString(matrix, colorDepth);
     inputStr.value = parseInt(str, colorDepth);
     IMG.draw(canvas, matrix);
+    // draw ver
+    ctx.fillStyle = 'lime';
+    ctx.textBaseline = 'top';
+    ctx.font = '10px arial';
+    ctx.fillText('v0.0.0', 5, canvas.height - 15);
 });
 // update from input element
 inputStr.addEventListener('keyup', function (e) {
---
+++ forpost/canvas-example-image-limits/todo.md
@@ -0,0 +1,6 @@
+# canvas-example-image-limits todo
+
+## 0.0.0 - basic idea
+* (done) get basic idea togetaher
+* (done) use createCnavas, and display ver number
+* make a pkg 0.0.0 folder
