43600c6840b2cf96a4b551bb47f347f0a9fb2a77
dustinpfister <dustin.pfister@gmail.com>
2021-02-27 14:49:44 -0500

    using new createCanvas method

--- forpost/canvas-example-kill-box/main.js
+++ forpost/canvas-example-kill-box/main.js
@@ -25,13 +25,9 @@ var states = {
 };
 
 // create canvas
-var canvas = document.createElement('canvas'),
-ctx = canvas.getContext('2d'),
-container = document.getElementById('canvas-app') || document.body;
-container.appendChild(canvas);
-canvas.width = 320;
-canvas.height = 240;
-ctx.translate(0.5, 0.5);
+var canvasObj = utils.createCanvas(),
+canvas = canvasObj.canvas,
+ctx = canvasObj.ctx;
 
 // Main State Machine Object
 var sm = {
