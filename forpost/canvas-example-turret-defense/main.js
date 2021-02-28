0a58f1b707b8e1e2c84312c2ef2a3c782b5f9f32
dustinpfister <dustin.pfister@gmail.com>
2021-02-28 15:15:17 -0500

    using create canvas method

--- forpost/canvas-example-turret-defense/main.js
+++ forpost/canvas-example-turret-defense/main.js
@@ -1,7 +1,6 @@
-var canvas = document.getElementById('the-canvas'),
-ctx = canvas.getContext('2d');
-canvas.width = 320;
-canvas.height = 240;
+var canvasObj = u.createCanvas(),
+canvas = canvasObj.canvas,
+ctx = canvasObj.ctx;
 
 var game = td.createGameObject();
 
