df5aa42537f904128c73839ac5e81cf4d0a6f2c1
dustinpfister <dustin.pfister@gmail.com>
2021-03-01 17:28:54 -0500

    state-machine.js clean up

--- forpost/canvas-example-state-machine/lib/state-machine.js
+++ forpost/canvas-example-state-machine/lib/state-machine.js
@@ -1,53 +1,5 @@
 var Machine = (function () {
 
-    // PARSE arguments
-
-    // Parse a container argument
-/*
-    var parseContainer = function (container) {
-        // if object assume element that is to be used as the container
-        if (typeof container === 'object' && container != null) {
-            return container;
-        }
-        // if string assume id
-        if (typeof container === 'string') {
-            return document.getElementById(container);
-        }
-        // if we get this far return document.body
-        return document.body;
-    };
-*/
-    // CANVAS
-
-    // create a canvas for the given state machine
-/*
-    var createCanvas = function (sm, w, h) {
-
-        sm.canvas = document.createElement('canvas');
-        sm.ctx = sm.canvas.getContext('2d');
-        sm.container.appendChild(sm.canvas);
-        sm.canvas.width = w || 320;
-        sm.canvas.height = h || 240;
-        // fill black for starters
-        sm.ctx.fillStyle = 'black';
-        sm.ctx.fillRect(0, 0, sm.canvas.width, sm.canvas.height);
-    };
-*/
-    // get canvas relative point
-/*
-    var getCanvasRelative = function (e) {
-        var canvas = e.target,
-        bx = canvas.getBoundingClientRect();
-        var x = (e.changedTouches ? e.changedTouches[0].clientX : e.clientX) - bx.left,
-        y = (e.changedTouches ? e.changedTouches[0].clientY : e.clientY) - bx.top;
-        return {
-            x: x,
-            y: y,
-            bx: bx
-        };
-    };
-*/
-
     // attach a canvas event
     var attachCanvasEvent = function (sm, DOMType, smType) {
         sm.canvas.addEventListener(DOMType, function (e) {
@@ -89,10 +41,7 @@ var Machine = (function () {
 
     // create a new state machine
     return function (container, w, h) {
-
-
         var canvasObj = utils.createCanvas();
-
         // state machine Object
         var sm = {
             currentState: null,
@@ -120,22 +69,17 @@ var Machine = (function () {
                 loop();
             }
         };
-
-
         // create canvas and attach event handlers
         //createCanvas(sm, w, h);
         attachAllCanvasEvents(sm);
-
         // main loop
         var loop = function () {
             requestAnimationFrame(loop);
             var stateObj = sm.states[sm.currentState] || {};
-
             // call top level tick
             if (stateObj.tick) {
                 stateObj.tick(sm);
             }
-
             // call mode tick
             if (stateObj.modes && sm.currentMode) {
                 var mode = stateObj.modes[sm.currentMode];
@@ -143,11 +87,8 @@ var Machine = (function () {
                     mode.tick(sm);
                 };
             }
-
         };
-
         return sm;
-
     };
 
 }
