e31b823f8f97180e482ea5fcd6f38a528d767c96
dustinpfister <dustin.pfister@gmail.com>
2021-03-02 09:51:21 -0500

    using create canvas method

--- forpost/canvas-example-star/main.js
+++ forpost/canvas-example-star/main.js
@@ -1,19 +1,16 @@
-var container = document.getElementById('canvas-app'),
-canvas = document.createElement('canvas'),
-ctx = canvas.getContext('2d');
-ctx.translate(0.5,0.5);
-canvas.width = 320;
-canvas.height = 240;
-container.appendChild(canvas);
+var canvasObj = utils.createCanvas(),
+canvas = canvasObj.canvas,
+ctx = canvasObj.ctx;
+
 
 var state = pool.createState(),
 lt = new Date();
+
 var loop = function () {
     var now = new Date(),
     t = now - lt,
     secs = t / 1000;
     requestAnimationFrame(loop);
-
     draw.background(ctx, canvas);
     state.pool.forEach(function(obj){
         ctx.fillStyle = 'green';
