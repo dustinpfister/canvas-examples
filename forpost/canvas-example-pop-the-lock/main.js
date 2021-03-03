201a1fb7a1ad349e257b2fd7c0f050cd4c54ae57
dustinpfister <dustin.pfister@gmail.com>
2021-03-03 14:23:19 -0500

    build folder for pop the lock clone

--- forpost/canvas-example-pop-the-lock/main.js
+++ forpost/canvas-example-pop-the-lock/main.js
@@ -105,7 +105,7 @@ var drawPTL = function (ptl, ctx, canvas) {
     // create and append canvas element, and get 2d context
     var canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d'),
-    container = document.getElementById('gamearea') || document.body;
+    container = document.getElementById('canvas-app') || document.body;
     container.appendChild(canvas);
     // set width and height
     canvas.width = 320;
