d664f5fa349a44d32893ce20b96806929c64680f
dustinpfister <dustin.pfister@gmail.com>
2021-03-08 08:38:08 -0500

    using create canvas

--- forpost/canvas-example-pointer-manager-mouse-and-touch/main.js
+++ forpost/canvas-example-pointer-manager-mouse-and-touch/main.js
@@ -1,11 +1,6 @@
-
-var canvas = document.createElement('canvas'),
-ctx = canvas.getContext('2d'),
-container = document.getElementById('gamearea') || document.body;
-container.appendChild(canvas);
-canvas.width = 320;
-canvas.height = 240;
-ctx.translate(0.5, 0.5);
+var canvasObj = utils.createCanvas(),
+canvas = canvasObj.canvas,
+ctx = canvasObj.ctx;
 
 var sm = {
     currentState: 'init',
