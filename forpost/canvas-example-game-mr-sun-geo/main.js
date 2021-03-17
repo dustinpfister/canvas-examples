da3fc88766405fca158ea3d49a6179b39b01cabe
dustinpfister <dustin.pfister@gmail.com>
2021-03-17 15:53:11 -0400

    using createCanvas method

--- forpost/canvas-example-game-mr-sun-geo/main.js
+++ forpost/canvas-example-game-mr-sun-geo/main.js
@@ -1,10 +1,6 @@
-var canvas = document.createElement('canvas'),
-ctx = canvas.getContext('2d'),
-container = document.getElementById('canvas-app') || document.body;
-container.appendChild(canvas);
-canvas.width = 320;
-canvas.height = 240;
-ctx.translate(0.5, 0.5);
+var canvasObj = utils.createCanvas(),
+canvas = canvasObj.canvas,
+ctx = canvasObj.ctx;
 
 var changeState = function (sm, stateKey, opt) {
     opt = opt || {};
