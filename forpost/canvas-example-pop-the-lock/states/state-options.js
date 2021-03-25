ed6ebb02de7339212b0a4e38340f967ee77f2577
dustinpfister <dustin.pfister@gmail.com>
2021-03-25 10:52:24 -0400

    debug mode started

--- forpost/canvas-example-pop-the-lock/states/state-options.js
+++ forpost/canvas-example-pop-the-lock/states/state-options.js
@@ -6,7 +6,12 @@
             // Buttons
             var x = sm.canvas.width * 0.87,
             y = sm.canvas.height * 0.03;
+            // back to title button
             stateMachine.spawnButton(sm, {x: x, y: y, w : 64}, 'start_state_title', 'Title');
+
+            x = sm.canvas.width * 0.05;
+            y = sm.canvas.height * 0.2;
+            stateMachine.spawnButton(sm, {x: x, y: y, w : 256}, 'toggle_smprop_debugMode', 'Debug Mode: ' + sm.debugMode);
 
             // setup a background
             sm.background = draw.createGradient(sm.ctx, sm.canvas, 0.75, [[0,'#cc0000'],[0.25,'orange'],[1,'yellow']]);
