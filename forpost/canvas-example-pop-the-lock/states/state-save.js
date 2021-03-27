842439ee5ee7fb654e767fe52379c5d6a46b915d
dustinpfister <dustin.pfister@gmail.com>
2021-03-27 12:02:04 -0400

    update slot buttons background helper

--- forpost/canvas-example-pop-the-lock/states/state-save.js
+++ forpost/canvas-example-pop-the-lock/states/state-save.js
@@ -4,28 +4,40 @@
     // and to also manage these save slots. utils.load actions are typicaly also
     // prefromed in main.js, and utils.save actions are preformed in 
     // in things like a game over state.
+
+    // update slot button backgrounds helper
+    var setSlotButtonBackgrounds = function(sm){
+        var slotIndex = 0;
+        while(slotIndex < 4){
+            var current = stateMachine.getButtonByAction(sm.buttons, 'set_slotindex_' + slotIndex);
+            current.data.fill = 'red';
+            if(slotIndex === sm.saveSlotIndex){
+                current.data.fill = 'yellow';
+            }
+            slotIndex += 1;
+        }
+    };
+
     stateMachine.load({
         key: 'save',
         init: function (sm) {
-            // Buttons
+            // BUTTONS
+            // title button
             var x = sm.canvas.width * 0.55,
             y = sm.canvas.height * 0.8;
             stateMachine.spawnButton(sm, {x: x, y: y}, 'start_state_title', 'Title');
-
+            // slot buttons
             var slotIndex = 0;
             y = sm.canvas.height * 0.3;
             while(slotIndex < 4){
                 var x = sm.canvas.width * (0.12 + (0.6 * (slotIndex / 3)));
-                var button = stateMachine.spawnButton(sm, 
+                stateMachine.spawnButton(sm, 
                     {x: x, y: y, w: 96, h: 128}, 
                     'set_slotindex_' + slotIndex, 'Slot ' + slotIndex);
-                button.data.fill = 'red';
-                if(slotIndex === sm.saveSlotIndex){
-                    button.data.fill = 'yellow';
-                }
                 slotIndex += 1;
             }
-
+            // update slot button backgrounds for first time
+            setSlotButtonBackgrounds(sm);
         },
         trans: function (sm, secs) {
             poolMod.update(sm.buttons, secs, sm);
@@ -44,8 +56,6 @@
                 }
                 if(parts[0] === 'set'){
                     if(parts[1] === 'slotindex'){
-                        var current = stateMachine.getButtonByAction(sm.buttons, 'set_slotindex_' + sm.saveSlotIndex);
-                        current.data.fill = 'red';
                         sm.saveSlotIndex = Number(parts[2]);
                         var highScores = utils.load(sm.appName, sm.saveSlotIndex);
                         if(highScores){
@@ -53,8 +63,8 @@
                         }else{
                             sm.highScores = {};
                         }
-                        current = stateMachine.getButtonByAction(sm.buttons, 'set_slotindex_' + sm.saveSlotIndex);
-                        current.data.fill = 'yellow';
+                        // update backgrounds
+                        setSlotButtonBackgrounds(sm);
                     }
                 }
             }
